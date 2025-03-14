79a06ab69d30148b058e059bd2a67470c79e81f9
Paulian Le <paulianle7@gmail.com>
2025-03-14 16:28:35 -0700

    Time: 0 ms (100%), Space: 28.2 MB (58.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // hash tables 
-            // grab the frequencies of each element 
-        unordered_map<int, int> hashTabFreqs;
+        // tech: hash table --> time comp: o(n)
+            // helps us count element occurences efficiently 
+            // key: val --> maps to value: count num of occ of that val 
+        
+        // 1. declare hash table 
+        unordered_map<int, int> numFreq; 
 
-        // iterate thr array 
-        for(int num : nums)
+        // 2. iterate thr array 
+        for(int val : nums)
         {
-            if(hashTabFreqs.find(num) != hashTabFreqs.end())
+            // check if not in hash table first 
+            if(numFreq.find(val) == numFreq.end())
             {
-                hashTabFreqs[num]++;
+                // not yet seen --> add to hash table 
+                numFreq[val] = 1; 
             }
             else 
             {
-                hashTabFreqs[num] = 1;
+                // val has been seen already --> increment freq (aka val of key-val pair)
+                numFreq[val] += 1; 
             }
         }
 
-        // find greatest freq - n / 2
-        for(auto pair : hashTabFreqs)
+        // 3. return highest val from the key-val pair 
+        for(auto pair : numFreq)
         {
             if(pair.second > nums.size() / 2)
             {
-                return pair.first;
+                return pair.first; // return the val w/ the highest majority 
             }
         }
 
