98a378e74c751b17a1382a3d906e1d43eba9e059
Paulian Le <paulianle7@gmail.com>
2025-03-14 17:40:12 -0700

    Time: 0 ms (100%), Space: 11.7 MB (7.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,33 +2,38 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         // tech: using 3 pointers in total --> time comp: o(n)
-            // p0 = rightmost boundary of 0s 
+            // p0 = rightmost boundary of 0s
             // curr = index of curr element 
-            // p2 = leftmost boundary of 2s 
+            // p2 = leftmost boundary of 2s
+        
+        // 1. declare ptrs to track rightmost boundary of 0s 
+            // if curr element is 0 --> then it'll be swapped w/ element at p0
+            // p0 and curr then ++...
+                // as the rightmost boundary of 0s has increased...
+                // so we need to shift accordingly
+        int p0 = 0;
+        int currPos = 0;
 
-        // 1. declare ptr to track rightmost boundary of 0s 
-            // if curr element is 0 --> then it'll be swapped w/ element at p0 
-            // p0 and curr then ++ 
-        int p0 = 0; 
-        int currPos = 0; 
-
-        // 2. declare ptr to track leftmost boundary of 2s 
-            // if curr element is 2 --. then it'll be swapped w/ element at p2 
-            // p2 will then be moved to the left -- 
+        // 2. declare ptr to track leftmost boundary of 2s
+            // if curr element is 2 --> then it'll be swapped w/ element at p2
+            // p2-- as need to increase leftmost boundary of 2s now 
         int p2 = nums.size() - 1;
 
+        // 3. iterate thr and change array accordingly
         while(currPos <= p2)
         {
-            if(nums[currPos] == 0) // when element is 0 
+            if(nums[currPos] == 0) // when curr element is 0 
             {
-                swap(nums[currPos++], nums[p0++]);
+                swap(nums[currPos++], nums[p0++]); 
             }
-            else if(nums[currPos] == 2) // when element is 2 
+            else if(nums[currPos] == 2) // when curr element is 2
             {
-                swap(nums[currPos], nums[p2--]);
+                swap(nums[currPos], nums[p2--]); 
             }
-            else // when element is 1 --> so no need to do any changes
+            else // when curr element is actually 1 
             {
+                // no need for any changes
+                // just update currPos 
                 currPos++;
             }
         }
