58c0dd7b053889b6f400e9cbbc869b1464454ba4
Paulian Le <paulianle7@gmail.com>
2025-03-16 23:57:46 -0700

    Time: 71 ms (9.71%), Space: 34.4 MB (8.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,52 +1,47 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // time comp: o(m * n) 
-            // o(m) - total num of input strings we're given
-            // o(n) - avg length of a string (as we're going thr each char in string)
+        // time comp: o(n * m) 
+            // m - total num of input strings we're given 
+            // n - avg length of each of those strings 
         
-        // tech: HASH MAP APPROACH
-            // key: stores freq of each char in string separated by a comma 
-                // abc --> 1, 1, 1, ......, 0
-            // value: stores list of strings that share that freq of each char
+        // tech - hash table 
+            // key - freq of each char in string separated by comma 
+            // val - stores list of strings that share the same freq of char's 
         
         // 1. declare and initialize hash table 
-        unordered_map<string, vector<string>> res;
+        unordered_map<string, vector<string>> res; 
 
-        // 2. iterate thr each respective string in the array of strings given 
-        for(const auto& s : strs)
+        // 2. iterate thr each string from the input array
+        for(const auto& s : strs) 
         {
-            // declare and initialize an array w/ 26 0's (each 0 will be for each char)
+            // declare / initialize array of 26 0's 
+                // each 0 indicaing freq of a char 
             vector<int> count(26, 0);
 
-            // == going thr each char in each string now! 
-                // count how much of each char shows up
-                // ex: see an a --> increment count[0] (which rep 'a') by 1
+            // iniitializing the hash table 
+                // see an a --> increment count[0] which reps 'a' by 1
             for(char c : s)
             {
-                // taking ascii val of curr char we're at and subtracting it by "a" 
-                    // ... gives us correct index to increment the value for
-                    // ... then we increment that index's value by 1 
+                    // taking ascii val of curr char subtrac. by 'a'
+                        // will give us correct index...
+                        // to incr. the value for 
                 count[c - 'a']++;
             }
 
-            // 3. create our key 
-                // add freq of letter "a" to our "key" string first 
-            string key = to_string(count[0]);
-            // iterate thr count array that contains freq of each char now
-            for(int i = 1; i < 26; i++)
-            {
-                // adds a comma then adds freq of next char in alphabet
-                key += ',' + to_string(count[i]);
+            // 3. then create key after grabbing freqs of each char 
+            string key;
+            for(int i = 0; i < 26; i++) {
+                key += to_string(count[i]) + ",";  // Ensures separation
             }
 
-            // add string to hash table now w/ key-val pair done!
             res[key].push_back(s);
         }
 
-        // 4. create a vector to return our vector of strings of anagrams
+        // 4. create vector to return everything now :)
+            // want specifically the values 
         vector<vector<string>> resAnagrams;
-        for(const auto& pair : res) 
+        for(const auto& pair : res)
         {
             resAnagrams.push_back(pair.second);
         }
