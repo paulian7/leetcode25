b460377d0a9b6965d3cb0d15afe119222222acbd
Paulian Le <paulianle7@gmail.com>
2025-03-17 21:51:53 -0700

    Time: 11 ms (33.54%), Space: 11 MB (43.52%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // time comp: o(n)
-            // brute force: o(n^2).. checking every single substr
-        // approach: HASH MAP and SLIDING WINDOW
-            // count num of occurences of each char
-            // length of window - count of most freq char
-            // = which gives us num of chars in our window that we need to replace to match our most freq char
+        // tech: hash tables & sliding window --> time comp: o(n) 
+        
+        // 1. declare hash table 
+            // key: char of alphabet (26 possible)
+            // val: freq of that char in string 
+        unordered_map<char, int> charFreq;
+        int result = 0;
+        
+        // 2. declare 2 pointers 
+        int leftPtr = 0; 
+        
+        // var to return as result 
+        int maxFreq = 0;
 
-            // 1. declare our hash map being used 
-                // key: character of the alphabet (26 possible)
-                // val: freq of that character in str
-            unordered_map<char, int> charFrequencies; 
-            int result = 0;
+        // rightPtr declared in for-loop as we iterate thr string
+        for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
+        {
+            // incrementing freq (the value) for each key
+            charFreq[s[rightPtr]]++;
 
-            // 2. declare / initialize our left and right ptrs for sliding window 
-            int leftPtr = 0; 
+            // update maxFreq 
+                // taking the maxFreq each time as we'll be...
+                // taking length of str - maxFreq --> which gives us # of replac...
+                // ...we need to do 
+            maxFreq = max(maxFreq, charFreq[s[rightPtr]]);
 
-            // declare maxFreq as well
-            int maxFreq = 0;
-
-            // rightPtr declared / initialized in for-loop
-            for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
+            // update window if necessary 
+                // checking if # of allowed replacements cover the needed replacements
+            while((rightPtr - leftPtr + 1) - maxFreq > k)
             {
-                // incres freq of chars accordingly in hash map 
-                charFrequencies[s[rightPtr]]++; 
+                // want to remove the freq of that value leftPtr pointed to from hash table
+                    // as we're no longer counting that freq
+                charFreq[s[leftPtr]]--;
 
-                // initialize maxFreq
-                    // gets most common freq in curr window
-                maxFreq = max(maxFreq, charFrequencies[s[rightPtr]]);
-
-                // sliding window! - ensure curr window is valid 
-                    // rightPtr - leftPtr + 1: gets length of str 
-                        // +1 ensures we get all elements in the range (including the ptrs)
-                while((rightPtr - leftPtr + 1) - maxFreq > k)
-                {
-                    // condition isn't valid :(... so need to update our leftPtr...
-                        // and subsequently our sliding window 
-                    charFrequencies[s[leftPtr]]--; // remove freq of char by 1 in hash map 
-                    leftPtr++; // incr leftPtr --> updates hash map
-                }
-
-                // updates result w/ max length of any valid sliding window 
-                result = max(result, rightPtr - leftPtr + 1); 
+                // update leftPtr --> move the window along 
+                leftPtr++;
             }
 
-            return result;
+            // updates result w/ max length of any valid sliding window
+            result = max(result, rightPtr - leftPtr + 1);
+        }
+
+        return result;
     }
 };
