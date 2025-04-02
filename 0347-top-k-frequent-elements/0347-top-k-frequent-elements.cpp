4afb8898ac6f8a3ffcc1f8f58c35492945e799b6
Paulian Le <paulianle7@gmail.com>
2025-04-01 17:19:34 -0700

    Time: 0 ms (100%), Space: 19.7 MB (14.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // tech: hash tables & using bucket sort algorithm
-            // time comp: o(n)
-        
+        // tech: hash tables & using bucket sort algo 
+            // time comp: o(n) 
+
         // 1. declare hash table 
-            // key: value; value: freq of that val
+            // key: value itself from array 
+            // val: freq of that specific val 
         unordered_map<int, int> countTable;
-        for(int num : nums)
+        // populate hash table 
+        for(int num : nums) 
         {
             countTable[num]++;
         }
 
-        // 2. create our freq bucket array 
-            // each index represents freq 
-            // so, want to +1 size of freq array as freq[4] needs an index 
-            // doing just the size of the array itself would make freq[4] go out of bounds
+        // 2. create our freq bucket 
+            // each index represents a freq 
+            // so, want to +1 size of freq array to account for ALL poss freqs w/o going out of bounds 
         vector<vector<int>> freq(nums.size() + 1);
-        for(const auto& pair : countTable) // iterate thr hash table 
+        for(const auto& pair : countTable)
         {
-            // pair.second is the index (aka freq) we want to add the val to
+            // pair.second is the index (aka freq), we want to add the val to 
             freq[pair.second].push_back(pair.first);
         }
 
-        // 3. collect top k freq elements to print out 
+        // 3. collect top k freq elements to print out now 
         vector<int> result; 
         for(int i = freq.size() - 1; i > 0; i--)
         {
