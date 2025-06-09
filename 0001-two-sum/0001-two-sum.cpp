c0b31e7fd0efb0a4068f1e42708aeb9da30209b6
Paulian Le <paulianle7@gmail.com>
2025-06-08 21:15:54 -0700

    Time: 5 ms (49.34%), Space: 14.9 MB (21.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // tech: hashing --> time comp: o(n) 
-            // want to track val and its index to return 
-        
+            // want to track val and its index to return
+
         // 1. declare hash table 
         unordered_map<int, int> hashTable; 
 
         // 2. iterate thr array 
         for(int i = 0; i < nums.size(); i++)
         {
-            // 3. find diff --> let's us iterate once thr the array 
+            // 3. find diff --> let's us iterate thr the array ONCE 
             int diff = target - nums[i]; 
 
-            // 4. search if found in hashTable already 
+            // 4. search if found in hashTable already --> o(1) constant time for lookup 
             if(hashTable.find(diff) != hashTable.end())
             {
                 return {i, hashTable[diff]}; 
             }
 
-            // else... add to hash table for later 
-            hashTable[nums[i]] = i;
+            // else... add to hash table for later lookup 
+            hashTable[nums[i]] = i; 
         }
 
-        return {-1, -1}; 
+        // worst case return nothing 
+        return {}; 
     }
 };
