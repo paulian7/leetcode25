32f8364359699f5593e9d8a8c0fb25f9ce625963
Paulian Le <paulianle7@gmail.com>
2025-06-11 15:30:29 -0700

    Time: 71 ms (10.8%), Space: 34.4 MB (10.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // time comp: o(n * m) 
-            // m - total num of input strings we're given 
-            // n - avg length of each of those strings 
-        
         // tech - hash table 
-            // key - freq of each char in the entirity of a string (separated by commas)
-            // val - storing list of strings that share that same freq string
+            // key - freq of each char in str (separated by commas) 
+            // val - storing list of strings that share that same freq string 
         
         // 1. declare & initialize the hash table 
         unordered_map<string, vector<string>> res; 
 
-        // 2. iterate thr each string from the input array 
+        // 2. iterate thr EACH STRING from the input array 
         for(const auto& s : strs)
         {
-            // declare / initialize an array of 26 0's 
+            // 3. declare / initialize an array of TWENTY-SIX 0's 
                 // each 0 indicating freq of a char 
-            vector<int> count(26, 0);
+            vector<int> count(26, 0); 
 
-            // initialize the hash table 
-                // see an a --> increment count[0] which reps 'a' by 1
-            for(char c : s)
+            // 4. initialize the hash table 
+                // see an a --> increment count[0] which reps 'a' by 1 
+            for(char c : s) 
             {
-                // taking ascii val of curr char subtractin' by 'a' 
-                    // will give us correct index! 
-                    // to incr the val for 
-                count[c - 'a']++;
+                // taking ascii val of curr char and subtracting it by 'a' 
+                    // helps us give correct index to incr val for 
+                    // ex: 'c' - 'a' --> 99 ascii val - 97 ascii val --> gives index 2 
+                count[c - 'a']++; 
             }
 
-            // 3. then create key after grabbing freq of each char IN THAT STRING
+            // 5. then create key after grabbing freq of char in THIS STRING 
             string key; 
             for(int i = 0; i < 26; i++)
             {
                 key += to_string(count[i]) + ","; 
             }
 
-            // take this key-val pair and add it to our hash table 
-            res[key].push_back(s);
+            // 6. take this key-val pair and add it to our hash table 
+            res[key].push_back(s); 
         }
 
-        // 4. create vector to specifically return the VALUES now 
-            // aka group of anagrams
+        // 7. after iterating thr all strings in input array....
+            // create vector to specifically return our RESULT! :D 
         vector<vector<string>> resAnagrams;
         for(const auto& pair : res)
         {
-            resAnagrams.push_back(pair.second);
+            resAnagrams.push_back(pair.second); 
         }
 
-        // return our answer! :)
+        // 8. finally return our answer :) 
         return resAnagrams;
     }
 };
