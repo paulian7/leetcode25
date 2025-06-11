9225289d02748cd7410b0f141b6a0d37c27eef24
Paulian Le <paulianle7@gmail.com>
2025-06-11 12:26:52 -0700

    Time: 0 ms (100%), Space: 10.1 MB (11.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // tech: hash tables
-            // time comp: o(n + m) --> as we're iterating thr 2 strings respectively 
+        // tech: hash tables --> time comp: o(n) 
 
-        // 1. cover base case -- 
-            // checking whether or not both strings are the same length 
-            // bc if not same length --> can't be anagrams :(
+        // base case 
         if(s.length() != t.length())
         {
             return false; 
         }
 
-        // 2. declare and initialize hash tables for each respective string 
-        unordered_map<char, int> sTable;
-        unordered_map<char, int> tTable;
+        // declare hash tables 
+        unordered_map<char, int> hashS; 
+        unordered_map<char, int> hashT; 
 
-        // iterate thr the strings to initialize hash table
+        // iterate thr both strings 
         for(int i = 0; i < s.length(); i++)
         {
-            sTable[s[i]]++;
-            tTable[t[i]]++;
+            hashS[s[i]]++; 
+            hashT[t[i]]++; 
         }
 
-        // 3. compare whether or not hash tables are equivalent! 
-            // equivalent --> dealing with anagrams! :D 
-        return sTable == tTable;
+        return hashS == hashT; 
     }
 };
