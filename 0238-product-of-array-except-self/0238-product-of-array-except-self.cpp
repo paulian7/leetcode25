84b44ee87a6cbc2ad002fdfa0f5086e5f99e6133
Paulian Le <paulianle7@gmail.com>
2025-06-13 14:22:19 -0700

    Time: 3 ms (38.76%), Space: 40.3 MB (44.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,39 +2,38 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         // tech: prefix * postfix --> time & space comp: o(n) 
-            // one pass forward for prefix 
+            // one pass forward to get the prefix 
             // one pass backwards for postfix 
-            // multiply BOTH together to get correct result 
+            // multiply BOTH together to get the correct result 
         
         // 1. declare vector of size nums
         vector<int> result(nums.size(), 1); // initialize to all default vals of 1
 
-        // 2. calculate prefix vals 
+        // 2. calc the prefix vals 
             // have prefix var set to 1 to start bc first element doesn't have prefix val
         int prefixVal = 1;
         for(int i = 0; i < nums.size(); i++)
         {
             // toss prefix into result vector first 
-            result[i] = prefixVal;
+            result[i] = prefixVal; 
 
-            // multiply prefix w/ each element as we go thr array 
+            // multiply prefix w/ each element as we go thr the array 
                 // as we want culmative product up to that point 
             prefixVal *= nums[i];
         }
 
-        // 3. calculate postfix vals 
+        // 3. calc the postfix vals 
         int postfixVal = 1;
         for(int i = nums.size() - 1; i >= 0; i--)
         {
-            // start multiplying each element w/ our postfix vals now 
-                // where we're multiplying both our pre AND postfix vals
+            // start multiplying each element w/ our postfix val RIGHT AWAY
+                // where we're multiplying both our PRE AND POSTFIX VALS at the same time 
             result[i] *= postfixVal;
 
-            // update postfix now w/ val in array
+            // update postfix w/ val in array 
             postfixVal *= nums[i];
         }
 
-        // returns updated array
         return result;
     }
 };
