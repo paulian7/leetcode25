ff68efa7deda762e2fd321499108fcca1d5be89a
Paulian Le <paulianle7@gmail.com>
2025-06-15 13:08:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,32 +2,34 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         // tech: hash sets --> time comp: o(n) 
+        
+        // 1. declare hash set 
+        unordered_set<int> numSet(nums.begin(), nums.end()); 
 
-        // 1. declare hash set
-        unordered_set<int> numSet(nums.begin(), nums.end());
-        int longest = 0;
+        // var to return at end - tracks longest consec 
+        int longest = 0; 
 
-        // 2. iterate thr array
-        for (int num : numSet) 
+        // 2. iterate thr the array 
+        for(int num : numSet)
         {
-            // check for start of seqs
-
-            // check if we can find the val that came before our curr val
-                // if can't... then this if-statement executes
-                // which means we found the start of a seq!
-            if (numSet.find(num - 1) == numSet.end()) 
+            // 3. want to check for the start of sequences 
+                // if we can find the val that comes before our curr val...
+                // ... that is NOT the start 
+                // otherwise, if we can't --> we found a start! 
+            if(numSet.find(num - 1) == nums.end()) 
             {
-                // start counting longest consec seq
-                int length = 1;
-                while (numSet.find(num + length) != numSet.end()) 
+                // so... we found the start of a sequence 
+                int currLength = 1; // currLength of seq 
+                while(numSet.find(num + 1) != nums.end())
                 {
-                    length++;
+                    currLength++; 
                 }
 
-                longest = max(longest, length);
+                // update length tracker
+                longest = max(longest, currLength); 
             }
         }
-        
-        return longest;
+
+        return longest; 
     }
 };
