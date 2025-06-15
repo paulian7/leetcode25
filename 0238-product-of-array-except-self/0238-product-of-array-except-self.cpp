9f832a471945be3adc9b895d7585269a7d86884f
Paulian Le <paulianle7@gmail.com>
2025-06-14 18:37:05 -0700

    Time: 4 ms (22.16%), Space: 40.1 MB (79.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        // tech: prefix * postfix --> time & space comp: o(n) 
-            // one pass forward to get the prefix 
-            // one pass backwards for postfix 
-            // multiply BOTH together to get the correct result 
+        // tech: prefix sum 
+            // prefix * postfix --> time & space comp: o(n) 
+                // one pass forward to get the prefix 
+                // one pass backwards to get the postfix 
+                // multiply BOTH to get the correct result 
         
-        // 1. declare vector of size nums
-        vector<int> result(nums.size(), 1); // initialize to all default vals of 1
+        // 1. declare ANS vector of size nums 
+        vector<int> result(nums.size(), 1); 
 
-        // 2. calc the prefix vals 
-            // have prefix var set to 1 to start bc first element doesn't have prefix val
-        int prefixVal = 1;
+        // 2. calc the prefix vals first 
+            // have prefix val set to 1 to start... 
+            // ... bc first element element doesn't have a prefix val 
+        int prefixVal = 1; 
         for(int i = 0; i < nums.size(); i++)
         {
-            // toss prefix into result vector first 
+            // toss prefix into res vector FIRST 
             result[i] = prefixVal; 
 
-            // multiply prefix w/ each element as we go thr the array 
-                // as we want culmative product up to that point 
-            prefixVal *= nums[i];
+            // UPDATE PREFIX VAL - 
+                // multiplying prefix w/ each element as we go thr the array 
+                // (WHY?) - as we want the culmulative product UP to that point 
+            prefixVal *= nums[i]; 
         }
 
-        // 3. calc the postfix vals 
-        int postfixVal = 1;
+        // 3. calc the POSTFIX vals 
+        int postfixVal = 1; 
         for(int i = nums.size() - 1; i >= 0; i--)
         {
             // start multiplying each element w/ our postfix val RIGHT AWAY
-                // where we're multiplying both our PRE AND POSTFIX VALS at the same time 
-            result[i] *= postfixVal;
+                // where we're multiplying both our PRE and POSTFIX vals at the same time 
+                // will get us the product of everything but curr element 
+            result[i] *= postfixVal; 
 
-            // update postfix w/ val in array 
-            postfixVal *= nums[i];
+            // update postfix val now w/ val in array 
+            postfixVal *= nums[i]; 
         }
 
-        return result;
+        return result; 
     }
 };
