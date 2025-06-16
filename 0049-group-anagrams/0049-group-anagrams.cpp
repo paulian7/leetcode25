1b1dca783de540d22e948564168170df77853eba
Paulian Le <paulianle7@gmail.com>
2025-06-16 13:12:21 -0700

    Time: 69 ms (11.73%), Space: 34.3 MB (10.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // tech - hash table 
-            // key - freq of each char in str (separated by commas) 
-            // val - storing list of strings that share that same freq string 
+        // anagrams - basically if you scramble the words 
+            // you can make the other word 
+            // chars should share same frequency 
         
-        // 1. declare & initialize the hash table 
-        unordered_map<string, vector<string>> res; 
+        // tech: hash tables 
+            // key: a string that details frequency of each respective character 
+            // value: all the strings that share that frequency 
+        
+        unordered_map<string, vector<string>> hashTab; 
 
-        // 2. iterate thr EACH STRING from the input array 
-        for(const auto& s : strs)
+        // 1. iterate thr array 
+        for(int i = 0; i < strs.size(); i++)
         {
-            // 3. declare / initialize an array of TWENTY-SIX 0's 
-                // each 0 indicating freq of a char 
-            vector<int> count(26, 0); 
+            // want to iterate through each string now within the overall array 
 
-            // 4. initialize the hash table 
-                // see an a --> increment count[0] which reps 'a' by 1 
-            for(char c : s) 
+            // want to also declare a vector 
+                // that tracks frequencies of each char 
+                // index denotes which char 
+                // char 0 --> represents freq count for "a" 
+            vector<int> count(26, 0); 
+            for(char c : strs[i])
             {
-                // taking ascii val of curr char and subtracting it by 'a' 
-                    // helps us give correct index to incr val for 
-                    // ex: 'c' - 'a' --> 99 ascii val - 97 ascii val --> gives index 2 
+                // using ascii trick here 
+                    // 'a' - 'a' --> index 0 --> increment corr index 
+                    // that represents freq count tracker for a 
+                    // 'b' - 'a' --> 98 - 97 --> 1 --> increment val in index 1 
                 count[c - 'a']++; 
             }
 
-            // 5. then create key after grabbing freq of char in THIS STRING 
+            // want to turn that vector of integers into a string to store as a key 
             string key; 
             for(int i = 0; i < 26; i++)
             {
                 key += to_string(count[i]) + ","; 
             }
 
-            // 6. take this key-val pair and add it to our hash table 
-            res[key].push_back(s); 
+            // add this to the hash table tracker now 
+            hashTab[key].push_back(strs[i]); 
         }
 
-        // 7. after iterating thr all strings in input array....
-            // create vector to specifically return our RESULT! :D 
-        vector<vector<string>> resAnagrams;
-        for(const auto& pair : res)
+        // then actually return our result 
+        // want to create yet another vector to help us with this 
+        vector<vector<string>> resAnagrams; 
+        for(auto const& pair : hashTab)
         {
             resAnagrams.push_back(pair.second); 
         }
 
-        // 8. finally return our answer :) 
-        return resAnagrams;
+        return resAnagrams; // return result! 
     }
 };
