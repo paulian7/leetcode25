c9aae2fc903f1bf156bb97cb2836c911a6a70d2d
Paulian Le <paulianle7@gmail.com>
2025-06-15 19:52:00 -0700

    Time: 41 ms (90.38%), Space: 29.1 MB (52.68%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // ?: want 3 different values that add up to 0
+        // ?: want 3 diff values that add up to 0 
 
-       // 1. sort as this helps us avoid duplicates 
-       sort(nums.begin(), nums.end());  
+        // 1. sort() - helps us avoid duplicates 
+            // does give us time comp w/ sorting of o(n logn)
+        sort(nums.begin(), nums.end()); 
 
-       // vector of arrays of triplets to return as answer 
-       vector<vector<int>> resultArray; 
+        // 2. declare vector of triplets to return as our answer 
+        vector<vector<int>> res; 
 
-       // 2. iterate thr array using a for-loop & inner while-loop
-            // for-loop deals w/ first element in the triplet 
-            // while loop deals w/ the other 2 elements in the triplet using 2 pointers
-
+        // 2. iterate thr array using a for-loop & inner while-loop 
+            // for-loop - deals w/ first element in triplet 
+            // while loop - deals w/ the other 2 elements in the triplet (using 2 pointers)
         for(int i = 0; i < nums.size(); i++)
         {
-            // check: for dups --> if dup... continue to next iteration 
-            if(i > 0 && nums[i] == nums[i - 1]) continue; 
+            // 3. check for dups first! 
+            if(i > 0 && nums[i] == nums[i - 1]) continue; // aka go to next iteration bc dup 
 
-            // 2 pointers method ----
-                // deals w/ 2 other vals in the triplet 
+            // 4. finally do our 2 pointers method for our other 2 vals in triplet 
             int leftPtr = i + 1; 
             int rightPtr = nums.size() - 1; 
             while(leftPtr < rightPtr)
@@ -27,30 +26,33 @@ public:
                 // calc sum first as conditional statement w/ the 2 pointers 
                 int currSum = nums[i] + nums[leftPtr] + nums[rightPtr]; 
 
-                // rightPtr check - if currSum too big --> need to lower val of rightPtr 
-                if(currSum > 0)
+                // update pointers accordingly to the sum 
+                if(currSum > 0) // rightPtr's val too big --> so decrement to find a smaller val 
                 {
-                    rightPtr--;
+                    rightPtr--; 
                 }
-                else if(currSum < 0) // too small --> need to increase val of leftPtr 
+                else if(currSum < 0) // leftPtr's val too small --> so increment to find bigger val 
                 {
                     leftPtr++; 
                 }
-                else // when our currSum actually equates to 0
+                else 
                 {
-                    resultArray.push_back({nums[i], nums[leftPtr], nums[rightPtr]}); 
+                    // currSum is ACTUALLY 0 
+                    res.push_back({nums[i], nums[leftPtr], nums[rightPtr]}); 
 
-                    leftPtr++;
-                    // ensure there's no duplicates after the update in pointers 
+                    // update pointers (really only need to do one though)
+                        // as our conditional if's will help us out w/ the rest 
+                    leftPtr++; 
+
+                    // ensure no dups after leftPtr updated 
                     while(leftPtr < rightPtr && nums[leftPtr] == nums[leftPtr - 1])
                     {
-                        leftPtr++;
+                        leftPtr++; 
                     }
                 }
             }
         }
 
-        return resultArray;
-    
+        return res; 
     }
 };
