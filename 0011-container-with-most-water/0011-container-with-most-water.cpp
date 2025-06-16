a9fcbd98e46f444f14eae3145473c697db47a2ea
Paulian Le <paulianle7@gmail.com>
2025-06-15 20:14:20 -0700

    Time: 0 ms (100%), Space: 62.9 MB (78.83%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,42 +3,43 @@ public:
     int maxArea(vector<int>& height) {
         // tech: 2 pointers approach - time comp: o(n) 
             // main goal: to maximize the length while also maximizing the height 
-            // have pointers be in their own respective sides
+                // have pointers be in their own respective sides 
         
         // 1. declare variable to return 
-        int result = 0;
+        int result = 0; 
 
         // 2. declare / initialize our 2 pointers 
         int leftPtr = 0; 
-        int rightPtr = height.size() - 1;
+        int rightPtr = height.size() - 1; 
 
-        // 3. iterate thr array now searching for the largest length 
-            // moving only the ptrs that's the smallest btwn the 2 ptrs
-                // as we're searching for the largest height
+        // 3. iterate thr array now searching for the longest length 
+            // moving only the ptrs that's the smallest btwn the 2 ptrs 
+            // bc we want the LARGEST height 
         while(leftPtr < rightPtr)
         {
             // 4. calc curr area: length * height 
                 // want smallest height in calc to avoid any overflowing 
-            int areaCalc = (rightPtr - leftPtr) * min(height[leftPtr], height[rightPtr]);
+            int areaCalc = (rightPtr - leftPtr) * min(height[leftPtr], height[rightPtr]); 
 
-            // update max area accordingly now after calculation 
+            // update max area accordingly now after calc 
             result = max(result, areaCalc); 
 
-            // now update ptrs to maximize heights 
+            // now update ptrs to maximize heights
             if(height[leftPtr] < height[rightPtr])
             {
-                leftPtr++;
+                leftPtr++; 
             }
             else if(height[rightPtr] < height[leftPtr])
             {
-                rightPtr--;
+                rightPtr--; 
             }
             else 
             {
-                rightPtr--; // when both ptrs same val.. just update 1
+                // when both ptrs same val in height --> can pick one to just update 
+                rightPtr--; 
             }
         }
 
-        return result;
+        return result; 
     }
 };
