53c2dbb9192a9ee8d48ec9b5d17b390df272eba1
Paulian Le <paulianle7@gmail.com>
2025-06-19 12:43:14 -0700

    Time: 0 ms (100%), Space: 9.7 MB (94.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // 2 pointers tech --> time comp: o(n) 
+        // RMBR - PLAINDROME READ SAME FORWARDS AND BACKWARDS 
+            // (excluding any spaces & nonalphanumeric characters)
+        // tech: 2 pointers from start and end --> then going to middle 
+            // time comp: o(n) 
 
+        // 1. initialize 2 pointers 
         int leftPtr = 0; 
         int rightPtr = s.length() - 1; 
 
-        // execute our 2 pointers method 
+        // 2. execute 2 ptrs technique 
+            // have left and rightPtrs meet @ the middle 
+            // if equivalent --> palindrome! 
         while(leftPtr < rightPtr)
         {
-            // account for nonalpha characters 
+            // check for nonalpha chars w/ LEFTPTR 
             while(leftPtr < rightPtr && !isalnum(s[leftPtr]))
             {
+                // then increment for an actual alphanum char 
                 leftPtr++; 
             }
 
+            // check for nonalpha chars w/ RIGHTPTR 
             while(rightPtr > leftPtr && !isalnum(s[rightPtr]))
             {
+                // then increment for an actual aphanum char 
                 rightPtr--; 
             }
 
-            // finally check if it's a palindrome 
+            // FINALLY check for equivalence -- after lowercasing the char 
             if(tolower(s[leftPtr]) != tolower(s[rightPtr]))
             {
-                return false; 
+                return false; // not a palindrome immediately 
             }
 
+            // update ptrs for next iteration 
             leftPtr++; 
             rightPtr--; 
         }
