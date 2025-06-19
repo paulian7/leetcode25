75c63a59cd8487aa1a3f9688c8d1100c54bc52ff
Paulian Le <paulianle7@gmail.com>
2025-06-19 13:04:38 -0700

    Time: 0 ms (100%), Space: 97.3 MB (87.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // ?: buy low, sell high --> want to get largest profit! 
+        // ?: buy low, sell high --> want to get the largest profit
 
         // tech: 2 pointers --> time comp: o(n) 
-            // will scan through array once 
-            // leftPtr - day we buy 
-            // rightPtr - day we sell 
+            // will scan thr array once 
+            // leftPtr - the day we buy 
+            // rightPtr - the day we sell 
         
         // 1. initialize 2 pointers 
-        int leftPtr = 0; // buy 
-        int rightPtr = 1; // sell 
+        int leftPtr = 0; 
+        int rightPtr = 1; 
 
-        int maxProfit = 0; // val to return at the end 
+        // val to return at end 
+        int maxProfit = 0; 
 
         // 2. iterate thr our array prices 
         while(rightPtr < prices.size())
         {
-            // check if we made any profit 
+            // check if we've made any profit   
+                // if left < right 
             if(prices[leftPtr] < prices[rightPtr])
             {
-                // profit calc (sellPrice - buyPrice)
+                // profit calc (sellfPrice - buyPrice)
                 int diff = prices[rightPtr] - prices[leftPtr]; 
 
-                // update maxProfit var accordingly 
-                maxProfit = max(diff, maxProfit); 
+                // update maxProfit variable accordingly 
+                maxProfit = max(maxProfit, diff); 
             }
             else 
             {
-                // want to update leftPtr bc we found lowest price in rightPtr 
+                // want to update leftPtr bc we found an EVEN...
+                    // ... lower price in rightPtr 
                 leftPtr = rightPtr; 
             }
 
-            // always want to update rightPtr as we iterate thr 
+            // always want to update rightPtr as we itterate through 
             rightPtr++; 
         }
 
