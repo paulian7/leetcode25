2c2027f9bfcfa2b61e2739f117a52afead8cc3df
Paulian Le <paulianle7@gmail.com>
2025-07-01 13:10:37 -0700

    Time: 15 ms (40.45%), Space: 14.3 MB (22.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // tech: hash sets & sliding window 
-            // time comp: o(n) --> length of input string 
-            // space comp: o(m) --> total num of unique chars 
+        // want longest substring w/o duplicate characters 
+            // hash sets are handy w/ identifying duplicate characters 
+            // inserting elements and removing elements take o(1) time -- very handy 
         
-        // ?: want longest subarray w/ no duplicates 
-            // want to use sets as they don't allow duplicates 
+        // want substring -- sliding window technique is handy as we can go thr the string 
+            // only tracking characters that are valid to our scenario 
         
-        // 1. declare a set 
-        unordered_set<char> charsVisited; 
+        // 1. declare hash set 
+        unordered_set<char> seenChars; 
 
-        // 2. declare / initialize our 2 pointers for SLIDING WINDOW technique 
+        // track longest substr length -- to return later at the end 
+        int longest = 0; 
+
+        // 2. start sliding window technique - by declaring pointers 
         int leftPtr = 0; 
 
-        // declare variable to return as our result 
-        int length = 0; 
-
-        // 3. iterate thr our given string 
-            // where our rightPtr is used as the ITERATOR for the for-loop 
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // check for duplicates / update window accordingly 
-            while(charsVisited.find(s[rightPtr]) != charsVisited.end())
+            while(seenChars.find(s[rightPtr]) != seenChars.end())
             {
-                // oh no! dup found! :( --> NEED TO UPDATE WINDOW
-                // take out val from leftPtr
-                charsVisited.erase(s[leftPtr]); 
-
-                // update leftPtr now to next val --> officially updates window 
+                // fix the window :( 
+                
+                // take out the val leftPtr 
+                seenChars.erase(s[leftPtr]); 
                 leftPtr++; 
             }
 
-            // if no dups found --> insert new val into hashSet 
-            charsVisited.insert(s[rightPtr]); 
+            // if can't find element.. first time seeing it --> need to add to our hashSet 
+                // aka the thing that's actually tracking the validity of our window
+            seenChars.insert(s[rightPtr]); 
 
-            // update our max substring length now 
-            length = max(length, rightPtr - leftPtr + 1); 
+            longest = max(longest, (rightPtr - leftPtr + 1)); 
         }
 
-        return length; 
+        return longest; 
     }
 };
