0bc5bb2449b65cae4c6a7a75d039411af64cabfd
Paulian Le <paulianle7@gmail.com>
2025-07-03 13:32:48 -0700

    Time: 0 ms (100%), Space: 40 MB (99.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,27 +3,28 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         // tech: prefix sum 
             // prefix * postfix --> time & space comp: o(n) 
-                // one pass forward to get the prefix 
-                // one pass backwards to get the postfix 
-                // multiply BOTH to get the correct result 
+                // one pass forward to get the prefix vals 
+                // one pass backwarxs to get the postfix 
+                // can multiply BOTH to get the correct result 
         
-        // 1. declare ANS vector of size nums 
+        // 1. declare vector to return as result 
         vector<int> result(nums.size(), 1); 
 
         // 2. calc the prefix vals first 
-            // have prefix val set to 1 to start... 
-            // ... bc first element element doesn't have a prefix val 
+            // have prefix val set to 1 to start 
+            // ... bc first element doesn't have a prefixVal 
         int prefixVal = 1; 
         for(int i = 0; i < nums.size(); i++)
         {
-            // toss prefix into res vector FIRST 
+            // toss prefix into res vector first 
             result[i] = prefixVal; 
 
-            // UPDATE PREFIX VAL - 
+            // actually update our prefix val 
                 // multiplying prefix w/ each element as we go thr the array 
                 // (WHY?) - as we want the culmulative product UP to that point 
             prefixVal *= nums[i]; 
         }
+
 
         // 3. calc the POSTFIX vals 
         int postfixVal = 1; 
@@ -31,8 +32,8 @@ public:
         {
             // start multiplying each element w/ our postfix val RIGHT AWAY
                 // where we're multiplying both our PRE and POSTFIX vals at the same time 
-                // will get us the product of everything but curr element 
-            result[i] *= postfixVal; 
+                // will get us the product of everything but curr element!! 
+            result[i] *= postfixVal;
 
             // update postfix val now w/ val in array 
             postfixVal *= nums[i]; 
