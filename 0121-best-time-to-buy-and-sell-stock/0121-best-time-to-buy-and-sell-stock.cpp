3078a5680ed0d64873cb363b516bff8876fde301
Paulian Le <paulianle7@gmail.com>
2025-07-03 13:59:23 -0700

    Time: 0 ms (100%), Space: 97.2 MB (98.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // array 
-        // want to find the largest difference in one pass --> time comp: o(n) 
-            // space comp: o(1) 
-        
-        int leftPtr = 0; // buy 
-        int rightPtr = 1; // sell 
+        // buy low, sell high 
+        // iterate thr our integer array once 
+            // trying to find the largest difference 
 
+        // time comp: o(n) 
+        // space comp: o(1) 
+        
+        // 2 pointers 
+        int leftPtr = 0; // buy
+        int rightPtr = 1; // sell!~ 
+
+        // variable to return at the very end 
         int maxProfit = 0; 
 
-        // iterate thr our input array 
+        // want to iterate to the end
         while(rightPtr < prices.size())
         {
-            // if any profit has been made? 
+            // check if we can have any profit? 
             if(prices[rightPtr] > prices[leftPtr])
             {
                 int diff = prices[rightPtr] - prices[leftPtr]; 
 
-                // update our result 
+                // update final ans of course to return
                 maxProfit = max(maxProfit, diff); 
-            } 
+            }
             else 
             {
-                leftPtr = rightPtr; 
+                // rightPtr is smaller :(
+                leftPtr = rightPtr;
             }
 
+            // always ensure we're updating the rightPtr 
+                // as that's what's processing each element individually as we go 
             rightPtr++; 
         }
 
