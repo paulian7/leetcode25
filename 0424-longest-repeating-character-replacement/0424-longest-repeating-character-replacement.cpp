03ee612668e86490fb068fb0af2342af089fbb0b
Paulian Le <paulianle7@gmail.com>
2025-07-11 15:55:30 -0700

    Time: 9 ms (35.62%), Space: 11 MB (18.57%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,56 +1,55 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // tech: hash tables & sliding window 
-            // time comp: o(n) -- 'n' is the length of the string 
-            // space comp: o(m) -- 'm' num of unique characters within string 
-        
-        // 1. declare hash table 
-            // key: char (26 possible within alphabet) 
-            // val: freq of that char within the string 
-        unordered_map<char, int> charFreq; 
-        
-        // declare var to return as result 
-        int result = 0;
+        // want to identify the highest frequency --> hash table 
+            // key: char
+            // val: freq of that resp char 
+        // sliding window 
+            // to track the longest substr we can get according to the conditions 
 
-        // 2. declare 2 ptrs - for window 
-        int leftPtr = 0; 
 
-        // have a helper variable 
-            // helps optimize time comp from o(26*n) to o(n) 
-            // no need to continuously iterate thr hash table to find highest freq 
+        // time comp: 
+            // o(n) 
+        // space comp: 
+            // o(n) 
+        
+        unordered_map<char, int> hashCount; 
+
+        // final ans to return 
+        int longest = 0; 
+
+        // have another helping integer variable 
+            // tracks the highest frequency 
         int maxFreq = 0; 
 
-        // 3. iterate thr string 
-            // rightPtr iterator here 
+        // sliding window 
+        int leftPtr = 0; 
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // incr freq for each char 
-            charFreq[s[rightPtr]]++;
+            // update our hash table tracking the frequencies 
+            hashCount[s[rightPtr]]++; 
 
-            // update maxFreq accordingly 
-                // contians highest freq we've seen so far 
-                // the determination of whether or not we shrink the window 
-            maxFreq = max(maxFreq, charFreq[s[rightPtr]]); 
+            // track to see if we've found the highest freq 
+                // helps us reduce the time complexity down 
+                // from continuously checking which char has the highest complexity 
+            maxFreq = max(maxFreq, hashCount[s[rightPtr]]); 
 
-            // update window if necessary ---- (if > k)
-                // take lengthWindow - maxFreq 
-                    // gives num of chars that isn't the char of maxFreq 
-                    // aka the num of chars we need to replace 
+            // check whether or not we need to update our window 
             while((rightPtr - leftPtr + 1) - maxFreq > k)
             {
-                // need to update window!! 
-                // remove freq of leftPtrs val 
-                charFreq[s[leftPtr]]--; 
-
-                // update window 
+                // need to update window bc the number of replacements we do need... isn't covered by k 
+                
+                // update freq count in leftPtr 
+                hashCount[s[leftPtr]]--; 
+                
+                // then update leftPtr itself to shrink window 
                 leftPtr++; 
             }
 
-            // update res accordingly w/ max length if found 
-            result = max(result, (rightPtr - leftPtr + 1)); 
+            // update our final answer 
+            longest = max(longest, (rightPtr - leftPtr + 1)); 
         }
 
-        return result;
+        return longest; 
     }
 };
