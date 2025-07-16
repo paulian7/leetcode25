b7af8219efb3a1d80ef7e40de48012d33e0900db
Paulian Le <paulianle7@gmail.com>
2025-07-16 11:28:51 -0700

    Time: 23 ms (19.65%), Space: 14.1 MB (36.29%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // substring --> sliding window technique 
-        // without repeating characters? 
-            // use hash set for easy reference on whether or not a char has repeated 
-        
-        // overall time comp: 
-        // space comp: 
-
         unordered_set<char> seenChars; 
 
-        // execute sliding window technique 
+        int length = 0; 
+
+        // 2 pointers method 
         int leftPtr = 0; 
-
-        // final var to return 
-        int longest = 0; 
-
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // check for any repeating characters at first 
+            // check for condition 
             while(seenChars.find(s[rightPtr]) != seenChars.end())
             {
-                // val has been seen within our hash set --> a repeat has been found 
-                // so we want to update our window!! 
-
-                // remove from hash set first 
+                // update our window & remove from hash table 
                 seenChars.erase(s[leftPtr]); 
-
-                // then update our leftPtr to shrink our sliding window 
                 leftPtr++; 
             }
 
-            // add to hash set 
             seenChars.insert(s[rightPtr]); 
 
-            longest = max(longest, (rightPtr - leftPtr + 1)); 
+            length = max(length, (rightPtr - leftPtr + 1)); 
         }
 
-        return longest; 
+        return length;
     }
 };
