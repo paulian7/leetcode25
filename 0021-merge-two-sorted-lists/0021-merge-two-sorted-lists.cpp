7025f6a7a34c3a456bcad597a85e953b48f373ce
Paulian Le <paulianle7@gmail.com>
2025-07-16 14:52:07 -0700

    Time: 0 ms (100%), Space: 19.5 MB (62.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,28 +11,28 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // tech: iterative approach --> time comp: o(n + m) 
-        
-        // 1. declare and initialize a dummy node
+        // tech: iterative approach --> time comp: o(n + m)
+
+        // 1. declare and initialize a dummy node 
             // helps us avoid edge cases of an empty linked list 
         
         // dummyNodePtr points to the dummy node we created w/ val of -1
-        ListNode *dummyNodePtr = new ListNode(-1); 
+        ListNode *dummyNodePtr = new ListNode(-1);
 
-        // have a tailPtr point to last node in our linked list 
-            // which is the dummyNode for now 
-        ListNode *tailPtr = dummyNodePtr; 
+        // have a tailPtr point to the last node in our linked list 
+            // which is dummy node for now 
+        ListNode *tailPtr = dummyNodePtr;
 
         // 2. iterate thr the 2 lists and create a new merged list of the 2 
             // as long as both lists are non-empty 
-        while((list1 != nullptr) && (list2 != nullptr))
+        while((list1 != nullptr) && (list2 != nullptr)) 
         {
-            // if l1 has smallest node so far
             if(list1 -> val < list2 -> val)
             {
                 // add l1 val to end of tail 
-                tailPtr -> next = list1; 
-                // update l1 pointer 
+                tailPtr -> next = list1;
+
+                // update l1 pointer
                 list1 = list1 -> next;
             }
             else 
@@ -41,22 +41,22 @@ public:
                 list2 = list2 -> next;
             }
 
-            // always update tail ptr after adding a new node to our new linked list
+            // always ensure you're updating tailptr after the addition of a new node
             tailPtr = tailPtr -> next;
         }
 
-        // 3. add any remaining nodes from any of the lists (if any)
+        // 3. add any remaining nodes from the lists (if any)
         if(list1 != nullptr)
         {
-            tailPtr -> next = list1; 
+            tailPtr -> next = list1;
         }
         else 
         {
-            tailPtr -> next = list2; 
+            tailPtr -> next = list2;
         }
 
-        // 4. returns next node after the dummyNode 
-            // gives us the new merged linked list
+        // 4. return next node after the dummyNode 
+            // which will give us our new 
         return dummyNodePtr -> next;
     }
 };
