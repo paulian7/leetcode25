e4ad25b3e7be409432ef00f9dfc0ab4e5eb379d9
Paulian Le <paulianle7@gmail.com>
2025-07-16 11:29:00 -0700

    Time: 23 ms (19.65%), Space: 14.3 MB (22.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // tech - hash sets & sliding window 
+            // "longest substring" - sliding window 
+            // "w/o repeating chars" - hash set 
+                // as checking for existence of elements is o(1) time 
+
+        // complexity analysis -
+            // time: o(n) - n is s.length() 
+            // space: o(m) - num of unique characters 
+
+        // 1. declare final var to return 
+        int longest = 0; 
+
+        // 2. declare hash set to track seen chars that are unique 
         unordered_set<char> seenChars; 
 
-        int length = 0; 
-
-        // 2 pointers method 
+        // 3. sliding window technique 
         int leftPtr = 0; 
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // check for condition 
+            // condition - check for any repeating chars @ first 
             while(seenChars.find(s[rightPtr]) != seenChars.end())
             {
-                // update our window & remove from hash table 
+                // need to update window.. repeat has been found! 
+                // remove repeat from hash set first 
                 seenChars.erase(s[leftPtr]); 
+
+                // update leftPtr to shrink sliding window 
                 leftPtr++; 
             }
 
+            // if unique char, add to hash set for later ref 
             seenChars.insert(s[rightPtr]); 
 
-            length = max(length, (rightPtr - leftPtr + 1)); 
+            // update final var to return 
+                // (rightPtr - leftPtr + 1) --> gives us curr substring length 
+            longest = max(longest, (rightPtr - leftPtr + 1)); 
         }
 
-        return length;
+        return longest; 
     }
 };
