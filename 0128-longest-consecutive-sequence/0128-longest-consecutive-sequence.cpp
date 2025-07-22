9f48ceefe849711d5339d8b895644de05b2e7d6f
Paulian Le <paulianle7@gmail.com>
2025-07-21 21:55:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // ?: find longest consec seq w/o sorting the array 
-            // sorting the array takes o(n logn time)
-            // we want o(n) time 
-        
-        // tech: hash sets 
-            // iterating thr array --> o(n) time 
-            // lookups for existence of a val --> o(1) time 
-        
-        // time & space comp: 
-            // time: o(n) 
-            // space comp: o(n) 
-        
-        // 1. declare variable to return as result variable 
+        // hash sets --> help us write an algo w/ runtime of o(n) 
+            // insert, remove, searching for an element --> all o(1) 
+        unordered_set<int> hashSet(nums.begin, nums.end()); 
+
+        // var to return @ the end 
         int longest = 0; 
 
-        // 2. declare hash set 
-        unordered_set<int> hashSet(nums.begin(), nums.end()); 
-
-        // 3. iterate thr array searching for longest consec seq 
+        // iterate thr set checking for start of sequences 
         for(int val : hashSet)
         {
-            // check if we found the start of a sequence 
             if(hashSet.find(val - 1) == hashSet.end())
             {
+                // start of seq found!! ..... sooooooo
                 int length = 1; 
                 while(hashSet.find(val + length) != hashSet.end())
                 {
