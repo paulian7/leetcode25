bea9a263d240b15f224c8e494c663968918fed1b
Paulian Le <paulianle7@gmail.com>
2025-07-23 15:57:29 -0700

    Time: 43 ms (86.39%), Space: 28.9 MB (90.19%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,69 +1,67 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // ?: want 3 diff values that add up to 0 
+        // tech: 
+            // want 3 vals that add up to 0
+            // want to ensure no duplicate triplets are found 
+        
+        // time & space comp: 
+            // time: o(n logn) + o(n^2)
+                // simplifies down to o(n^2) 
+                // sort() - o(nlogn) 
+                // nested loops - o(n^2) 
+            // space: 
+                // if you count sort: o(1) 
+                    // c++ does in-place 
+                // output array: o(m) 
+                    // m - num of triplets
+        
+        // 1. sort! --> time comp; o(n logn) :(
+        sort(nums.begin(), nums.end());
 
-        // 1. sort() - helps us avoid duplicates 
-            // does give us time complexity of o(nlogn) as we're sorting 
-        sort(nums.begin(), nums.end()); 
+        vector<vector<int>> result; // will hold our final answer of triplets
 
-        // 2. declare vector of triplets to return as our answer 
-        vector<vector<int>> res; 
-
-        // 3. iterate thr array using a for-loop & inner while-loop 
-            // for-loop -- deals w/ first element in triplet 
-            // while loop - deals w/ the other 2 elements in the triplet 
-                // using the 2 pointers method here! 
+        // 2. start iterating thr 
         for(int i = 0; i < nums.size(); i++)
         {
-            // === 4. DEALING WITH FIRST VAL WITHIN TRIPLET == 
-            // check for dups first 
-            if(i > 0 && nums[i] == nums[i - 1])
-            {
-                // found duplicate! -- want to skip to next iteration 
-                    // (as we want to avoid dups in our triplet
-                continue; 
-            }
+            // deal w/ the first val in our triplet first 
+            if(i > 0 && nums[i] == nums[i - 1]) continue; // skip to next iteration bc dup
 
-            // == 5. DEALING W/ OTHER 2 VALS WITHIN TRIPLET == 
+            // otherwise, deal w/ our other 2 vals now 
+                // will be executing the 2 pointers method here to do so 
             int leftPtr = i + 1; 
             int rightPtr = nums.size() - 1; 
             while(leftPtr < rightPtr)
             {
-                // calc sum first as a conditional statement w/ the 2 pointers 
+                // get the curr triplet val 
                 int currSum = nums[i] + nums[leftPtr] + nums[rightPtr]; 
 
-                // update ptrs accordingly to the sum 
+                // adjust the pointers accordingly (based on the currSum)
                 if(currSum > 0)
                 {
-                    // rightPtr's val too big --> so decrement to find smaller val 
-                    rightPtr--; 
+                    rightPtr--; // needs to lower down bc it's pointing at a too big val
                 }
                 else if(currSum < 0)
                 {
-                    // leftPtr's val too small --> so increment to find larger val 
-                    leftPtr++; 
+                    leftPtr++; // need to increase leftPtr bc it's pointing at too small
                 }
                 else 
                 {
-                    // currSum is FINALLY 0! <-- what we want! 
+                    // we finally have a sum of 0! add it to our result vector 
+                    result.push_back({nums[i], nums[leftPtr], nums[rightPtr]});
 
-                    // add to res vector to store our VALID TRIPLET 
-                    res.push_back({nums[i], nums[leftPtr], nums[rightPtr]}); 
-
-                    // update pointers -- just need to update 1 
-                        // as our conditional statements above will udpate accordingly for us 
+                    // now update our pointers -- dont need to necessarily update both
                     leftPtr++; 
 
-                    // ensure no duplicates in the leftPtr 
+                    // ensure after the update of leftPtr... we haven't hit a dup! :(
                     while(leftPtr < rightPtr && nums[leftPtr] == nums[leftPtr - 1])
                     {
-                        leftPtr++; 
+                        leftPtr++;
                     }
                 }
             }
         }
 
-        return res;
+        return result; 
     }
 };
