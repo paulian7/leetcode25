a39493fc7397a0daa427728383144eac6ecd9a6f
Paulian Le <paulianle7@gmail.com>
2025-07-30 13:04:41 -0700

    Time: 4 ms (17.99%), Space: 97.4 MB (60.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // buy low, sell high 
-        // iterate thr our integer array once 
-            // trying to find the largest difference 
+        // tech: 
+            // want to find largest difference in one-pass 
+            // 2 pointers type method 
 
-        // time comp: o(n) 
-        // space comp: o(1) 
+        // time & space comp:
+            // time: o(n)
+                // everything behaving in a linear fashion 
+                // no pointers ever go backwards
+            // space: o(1) -- just using pointers (isn't much in comparison to a whole array)
         
-        // 2 pointers 
-        int leftPtr = 0; // buy
-        int rightPtr = 1; // sell!~ 
+        // 1. start 2 pointers method -- starting vals for now! 
+        int leftPtr = 0; // buy 
+        int rightPtr = 1; // when we sell! 
 
-        // variable to return at the very end 
+        // 2. start doing our one-pass 
+            // rightPtr acts as our type of iterator! 
         int maxProfit = 0; 
-
-        // want to iterate to the end
         while(rightPtr < prices.size())
         {
-            // check if we can have any profit? 
+            // check if we reached a certain condition 
+                // aka if we can find a difference
             if(prices[rightPtr] > prices[leftPtr])
             {
                 int diff = prices[rightPtr] - prices[leftPtr]; 
 
-                // update final ans of course to return
+                // update maxProfit 
                 maxProfit = max(maxProfit, diff); 
             }
             else 
             {
-                // rightPtr is smaller :(
-                leftPtr = rightPtr;
+                // need to update pointers bc we found an even smaller val in rightPtr's 
+                leftPtr = rightPtr; 
             }
 
-            // always ensure we're updating the rightPtr 
-                // as that's what's processing each element individually as we go 
+            // always update rightPtr to move things along 
             rightPtr++; 
         }
 
