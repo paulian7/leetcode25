9af3ead91eef5e2dbea7e561bb694f7c7803d718
Paulian Le <paulianle7@gmail.com>
2025-07-30 13:05:16 -0700

    Time: 0 ms (100%), Space: 97.4 MB (60.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // tech: 
-            // want to find largest difference in one-pass 
-            // 2 pointers type method 
-
-        // time & space comp:
-            // time: o(n)
-                // everything behaving in a linear fashion 
-                // no pointers ever go backwards
-            // space: o(1) -- just using pointers (isn't much in comparison to a whole array)
+        // ?: want the biggest difference 
+            // buy low, sell high
         
-        // 1. start 2 pointers method -- starting vals for now! 
-        int leftPtr = 0; // buy 
-        int rightPtr = 1; // when we sell! 
+        // tech: 2 pointers 
+        
+        // time & space comp: 
+            // time: o(n)
+            // space: o(1) 
 
-        // 2. start doing our one-pass 
-            // rightPtr acts as our type of iterator! 
+        // 1. declare variable to return as our final result 
         int maxProfit = 0; 
+
+        // 2. execute 2 pointers method 
+        int leftPtr = 0; // buy 
+        int rightPtr = 1; // sell 
+
+        // rightPtr is what will mainly be moving forward 
         while(rightPtr < prices.size())
         {
-            // check if we reached a certain condition 
-                // aka if we can find a difference
+            // CONDITION - check if a profit can be made 
             if(prices[rightPtr] > prices[leftPtr])
             {
                 int diff = prices[rightPtr] - prices[leftPtr]; 
 
-                // update maxProfit 
                 maxProfit = max(maxProfit, diff); 
             }
             else 
             {
-                // need to update pointers bc we found an even smaller val in rightPtr's 
+                // otherwise, need to update leftPtr bc found an even smaller val w/ rightPtr 
                 leftPtr = rightPtr; 
             }
 
-            // always update rightPtr to move things along 
+            // always update rightPtr at the end to prepare for next iteration 
             rightPtr++; 
         }
 
