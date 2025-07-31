d122c71b0df849fab7c51ad80c39737b0d753b6c
Paulian Le <paulianle7@gmail.com>
2025-07-31 14:17:23 -0700

    Time: 0 ms (100%), Space: 19.4 MB (87%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,49 +11,46 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // 1. create a dummy node 
-            // helps us avoid any edge cases w/ dealing w/ 
-            // empty linked lists 
-        ListNode *dummyNodePtr = new ListNode(-1); 
+        // tech: pointers
 
-        // 2. have another pointer that points to the end of 
-            // our new MERGED linked list 
-        ListNode *tailPtr = dummyNodePtr; 
+        // time & space comp: 
+            // time: o(n + m)
+            // space: o(1)
 
-        // 3. start merging the 2 sorted lists in place accordingly
-            // time comp: o(n + m) 
-                // n - length of list1 
-                // m - length of list 2 
-            // space comp: o(1) <-- all done in place other than use of pointers 
+        // 1. dummyNode pointer to help us w/ edge cases
+        ListNode *dummyNodePtr = new ListNode(-1);
+
+        // 2. have a tailPtr point to the end of our new list
+        ListNode *tailPtr = dummyNodePtr;
+
+        // 3. start iterating thr the 2 lists
         while((list1 != nullptr) && (list2 != nullptr))
         {
             if(list1 -> val < list2 -> val)
             {
                 tailPtr -> next = list1; 
-
-                list1 = list1 -> next; 
+                list1 = list1 -> next;
             }
             else
             {
                 tailPtr -> next = list2; 
-
                 list2 = list2 -> next; 
             }
 
-            // want to update our "iterator" 
+            // always ensure you're updating our tailPtr for next iteration
             tailPtr = tailPtr -> next; 
         }
 
-        // make sure both lists are exhausted 
+        // 4. ensure all lists have been iterated thr 
         if(list1 != nullptr)
         {
             tailPtr -> next = list1; 
         }
         else if(list2 != nullptr)
         {
-            tailPtr -> next = list2; 
+            tailPtr -> next = list2;
         }
 
-        return dummyNodePtr -> next; 
+        return dummyNodePtr -> next;
     }
 };
