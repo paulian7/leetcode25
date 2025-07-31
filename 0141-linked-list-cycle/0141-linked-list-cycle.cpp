aa5ce3155eb2eb9fc755e67977aae7d06ead1e36
Paulian Le <paulianle7@gmail.com>
2025-07-31 14:19:55 -0700

    Time: 8 ms (80.96%), Space: 11.9 MB (53.06%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,41 +9,24 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // tech: 2 pointers approach! 
-            // will have fast & slow ptr 
-                // fast: moves 2 steps at a time 
-                // slow: moves 1 step at a time 
-            // if no cycle: 
-                // fastPtr will eventually reach the end -> return false; 
-            // if cycle: 
-                // fastPtr will eventually meet up w/ slowPtr -> return true; 
-        
-        // time & space comp: 
-            // time: o(n) 
-            // space: o(1)
-        
-        
-        // 1. declare & initalize our 2 ptrs (default to head first)
-        ListNode *fastPtr = head; 
-        ListNode *slowPtr = head;
+        // time: o(n) 
+        // space: o(1)
 
-        // 2. start our 2 ptrs strategy 
-            // continue while loopin' until: 
-                // fastPtr is valid && isn't the last node in the list
+        // 1. create 2 ptrs
+        ListNode *slowPtr = head; 
+        ListNode *fastPtr = head; 
+
         while(fastPtr && fastPtr -> next)
         {
-            // update ptrs accordingly 
-            slowPtr = slowPtr -> next; 
+            slowPtr = slowPtr -> next;
             fastPtr = fastPtr -> next -> next; 
 
-            // check for cycle now! 
             if(slowPtr == fastPtr)
             {
-                return true; // cycle exists
+                return true;
             }
         }
 
-        // 3. return false if fastPtr reaches the end
         return false;
     }
 };
