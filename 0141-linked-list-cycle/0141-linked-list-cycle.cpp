9f42883355675a55dcb371809526881d22685cc9
Paulian Le <paulianle7@gmail.com>
2025-07-30 23:53:04 -0700

    Time: 4 ms (97.38%), Space: 11.7 MB (80.05%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,35 +9,41 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // tech: 2 ptrs approach --> time comp: o(n) 
-            // space comp: o(1), constant time 
-
-            // fast ptr: moves 2 steps at a time 
-            // slow ptr: moves 1 step at a time 
+        // tech: 2 pointers approach! 
+            // will have fast & slow ptr 
+                // fast: moves 2 steps at a time 
+                // slow: moves 1 step at a time 
+            // if no cycle: 
+                // fastPtr will eventually reach the end -> return false; 
+            // if cycle: 
+                // fastPtr will eventually meet up w/ slowPtr -> return true; 
         
-        // if no cycle: 
-            // fast ptr will eventually reach the end -> false 
-        // if cycle: 
-            // fast ptr will eventually meet up w/ slow ptr -> true
+        // time & space comp: 
+            // time: o(n) 
+            // space: o(1)
+        
+        
+        // 1. declare & initalize our 2 ptrs (default to head first)
+        ListNode *fastPtr = head; 
+        ListNode *slowPtr = head;
 
-        // 1. initialize pointers 
-        ListNode *slowPtr = head; 
-        ListNode *fastPtr = head;
-
-        // 2. iterate thr linked list as long as fastPtr hasn't reached end 
+        // 2. start our 2 ptrs strategy 
+            // continue while loopin' until: 
+                // fastPtr is valid && isn't the last node in the list
         while(fastPtr && fastPtr -> next)
         {
             // update ptrs accordingly 
-            slowPtr = slowPtr -> next;
+            slowPtr = slowPtr -> next; 
             fastPtr = fastPtr -> next -> next; 
 
-            // check for cycle (aka when slow == fast)
+            // check for cycle now! 
             if(slowPtr == fastPtr)
             {
-                return true; // cycle exists!
+                return true; // cycle exists
             }
         }
 
+        // 3. return false if fastPtr reaches the end
         return false;
     }
 };
