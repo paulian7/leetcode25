135332a1bb2d5967bf569d12c7e9bc4f897dde2c
Paulian Le <paulianle7@gmail.com>
2025-08-02 00:58:13 -0700

    Time: 0 ms (100%), Space: 14.2 MB (12.4%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,47 +1,54 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // tech: binary search - time comp: o(logn)
+        // tech: 
+            // bc array was sorted --> hints BINARY SEARCH should be used
+            // will be doing binary search w/ a twist
+            // will identify pivot within array 
         
-        // 1. declare & initialize our result variable to return 
-            // initialize to first element in array for now
-        int result = nums[0]; 
+        // time & space comp: 
+            // time: o(logn)
+            // space: 
 
-        // 2. declare and initialize our 2 pointers 
+        // 1. declare & initialize our result var to return 
+            // have default val to first element in array fOR NOW
+        int result = nums[0];
+
+        // 2. declare & initialize our 2 pointers 
         int leftPtr = 0; 
-        int rightPtr = nums.size() - 1;
+        int rightPtr = nums.size() - 1; 
 
-        // 3. iterate thr our array now 
-            // <= - the equal part ensures we cover all values 
+        // 3. iterate thr our array now
         while(leftPtr <= rightPtr)
         {
             // CASE 1 == CHECK IF ARRAY ALREADY SORTED BY DEFAULT
             if(nums[leftPtr] < nums[rightPtr])
             {
-                result = min(result, nums[leftPtr]);
-                break; // breaks out of while-loop ENTIRELY 
+                result = min(result, nums[leftPtr]); 
+                break; // BREAKS OUT OF WHILE-LOOP ENTIRELY
             }
 
-            // CASE 2 - WHEN ARRAY HAS BEEN ROTATED...
+            // CASE 2 - WHEN ARRAY HAS BEEN ROTATED..
+                // need to find mindpoint -- what binary search does.. divides search space in half
+                    // l + (r - l) / 2 
+                        // ^ calc midPt 
+            int midPt = leftPtr + (rightPtr - leftPtr) / 2; 
+            // check if midPt could poss be smallest val
+            result = min(result, nums[midPt]); 
 
-            // find midpoint first 
-                // l + (r - l) / 2; <- formula to calc midPt in binary search 
-                    // helps avoid overflow 
-            int midPoint = leftPtr + (rightPtr - leftPtr) / 2;
-            // check if midPoint could possibly be smallest val
-            result = min(result, nums[midPoint]); 
-
-            // now decide if we search either the left chunk or right chunk
-            if(nums[midPoint] >= nums[leftPtr]) // midPtr part of left sorted portion (big vals)
+            // NOW decide if we search LEFT or RIGHT sections
+            if(nums[midPt] >= nums[leftPtr]) // midPt is apart of left sorted portion (the big vals)
             {
-                // so we want to only search the right chunk 
-                // update left val accordingly 
-                leftPtr = midPoint + 1;
+                // so we want to search right portion (bc they should contain the smaller vals)
+                    // update leftPtr then to go to smaller vals portion 
+                leftPtr = midPt + 1;
             }
-            else // we're dealing with the right sorted portion (little baby values)
+            else 
             {
-                // want to search left chunk
-                rightPtr = midPoint - 1;
+                // we're dealing w/ the right sorted portion... so we want to search left...
+                    // bc anything from searching right would just increase..
+                    // and we don't want increasing vals
+                rightPtr = midPt - 1;
             }
         }
 
