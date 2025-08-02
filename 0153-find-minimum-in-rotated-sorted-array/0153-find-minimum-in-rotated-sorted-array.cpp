9ec35909e11b4ef2ceb76ac75bbe2bb226cdb110
Paulian Le <paulianle7@gmail.com>
2025-08-02 16:58:55 -0700

    Time: 0 ms (100%), Space: 14.2 MB (12.33%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,56 +2,57 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         // tech: 
-            // bc array was sorted --> hints BINARY SEARCH should be used
-            // will be doing binary search w/ a twist
-            // will identify pivot within array 
-        
+            // array sorted, rotated.. binary search! 
         // time & space comp: 
-            // time: o(logn)
-            // space: o(1)
+            // time: o(logn) <-- time comp of binary search
+            // space: o(1) 
+        
+        // 1. declare & initialize our result var to return
+            // have default val to first element in array FOR NOW
+        int result = nums[0]; 
 
-        // 1. declare & initialize our result var to return 
-            // have default val to first element in array fOR NOW
-        int result = nums[0];
+        // 2. declare & initialize our 2 pointers to prep for binary search
+        int leftPtr = 0; // smallest val
+        int rightPtr = nums.size() - 1; // largest val
 
-        // 2. declare & initialize our 2 pointers 
-        int leftPtr = 0; 
-        int rightPtr = nums.size() - 1; 
-
-        // 3. iterate thr our array now
+        // 3. BEGIN BINARY SEARCH! ===
         while(leftPtr <= rightPtr)
         {
-            // CASE 1 == CHECK IF ARRAY ALREADY SORTED BY DEFAULT
+            // CASE 1 == CHECK IF ARRAY SORTED ALREADY BY DEFAULT 
             if(nums[leftPtr] < nums[rightPtr])
             {
-                result = min(result, nums[leftPtr]); 
+                result = min(result, nums[leftPtr]);
                 break; // BREAKS OUT OF WHILE-LOOP ENTIRELY
             }
 
-            // CASE 2 - WHEN ARRAY HAS BEEN ROTATED..
-                // need to find mindpoint -- what binary search does.. divides search space in half
+            // CASE 2 == WHEN ARRAY HAS BEEN ROTATED..
+                // need to find midpt - what binary search does 
+                    // divides search area in half
                     // l + (r - l) / 2 
-                        // ^ calc midPt 
-            int midPt = leftPtr + (rightPtr - leftPtr) / 2; 
-            // check if midPt could poss be smallest val
-            result = min(result, nums[midPt]); 
+                        // ^ calculates the midPt 
+            int midPt = leftPtr + (rightPtr - leftPtr) / 2;
 
-            // NOW decide if we search LEFT or RIGHT sections
-            if(nums[midPt] >= nums[leftPtr]) // midPt is apart of left sorted portion (the big vals)
+            // check if midPt could poss be smallest val
+            result = min(result, nums[midPt]);
+
+            // now decide if we search LEFT or RIGHT section 
+            if(nums[midPt] >= nums[leftPtr]) 
             {
-                // so we want to search right portion (bc they should contain the smaller vals)
-                    // update leftPtr then to go to smaller vals portion 
+                // midPt is apart of left sorted portion...(the big vals)
+                // SO, we want to search the right portion 
+                    // bc they should contian the smaller vals
+                // update leftPtr then to go to the smaller vals portion 
                 leftPtr = midPt + 1;
             }
             else 
             {
-                // we're dealing w/ the right sorted portion... so we want to search left...
-                    // bc anything from searching right would just increase..
+                // we're dealing w/ the right sorted portion, so we want to search left
+                    // bc anything from searching right would just increase... 
                     // and we don't want increasing vals
                 rightPtr = midPt - 1;
             }
         }
-
+        
         return result;
     }
 };
