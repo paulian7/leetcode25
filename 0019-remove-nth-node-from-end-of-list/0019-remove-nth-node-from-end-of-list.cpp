8a213d7853e536e7a613130673b43a0f3d1e57f0
Paulian Le <paulianle7@gmail.com>
2025-08-04 23:49:28 -0700

    Time: 0 ms (100%), Space: 15 MB (24.74%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,44 +11,46 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        // tech: 2 pointers && dummyNode trick
+        // tech: 2 pointers && dummyNode trick 
 
         // time & space comp: 
-            // time: o(n)
-            // space: o(1)
+            // time: o(n) 
+            // space: o(1) 
         
         // 1. create dummyNode && have ptr point to it 
-            // create dummy node w/ dummy val of 0 
-            // have next ptr point to HEAD
-        ListNode *dummyPtr = new ListNode(0, head);
+            // creating dummyNode w/ dummy val of 0 
+            // have its next ptr point to HEAD
+        ListNode *dummyPtr = new ListNode(0, head); 
 
         // 2. have left & rightPtrs 
-            // rightPtr - acts as our fastPtr, but default its val to HEAD for now
-        ListNode *leftPtr = dummyPtr;
-        ListNode *rightPtr = head; 
+            // rightPtr - acts as our fastPtr - but default its val to HEAD for now
+            // leftPtr - will lag behind & pt to nullptr for now
+        ListNode *leftPtr = dummyPtr; 
+        ListNode *rightPtr = head;
 
-        // 3. place rightPtr @ correct spot to prepare 
-            // rightPtr will be 1 node BEHIND the node we need to remove after this while()
+        // 3. place rightPtr & corr spot to prepare 
+            // rightPtr will be 1 node BEHIND the node we need to remove...
+                // .. after this while() 
         while(n > 0)
         {
-            rightPtr = rightPtr -> next;
-            n -= 1;
+            rightPtr = rightPtr -> next; 
+            n -= 1; 
         }
 
-        // 4. utilize the 2 of our pointers now til rightPtr reaches end 
-            // this will help us pos leftPtr in the corr spot to do the removal next 
+        // 4. utilize the 2 of our ptrs til rightPtr reaches end 
+            // will pos leftPtr to corr spot to do the removal 
         while(rightPtr != nullptr)
         {
             leftPtr = leftPtr -> next; 
-            rightPtr = rightPtr -> next;
+            rightPtr = rightPtr -> next; 
         }
 
-        // 5. do the REMOVAL of the nth node now
-            // NOW that leftPtr is the correct spot
-            // leftPtr is curr RIGHT BEHIND the node we need to remove 
-        leftPtr -> next = leftPtr -> next -> next; 
+        // 5. DO the REMOVAL of the nth node NOW
+            // NOW... that the leftPtr is in the corr spot 
+            // leftPtr is currently RIGHT BEHIND THE node we need to remove 
+        leftPtr -> next = leftPtr -> next -> next;
 
-        // 6. return corrected linked list w/o the node 
+        // 6. use dummyPtr -> next to return corr list w/o the nth node
         return dummyPtr -> next;
     }
 };
