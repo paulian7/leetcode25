25a4f8526c6ff6fd7d188d9f01a78e5c01bfd1da
Paulian Le <paulianle7@gmail.com>
2025-08-05 23:23:40 -0700

    Time: 4 ms (39.12%), Space: 9.8 MB (48.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // tech: anagrams! 
-            // hash tables! 
-                // key: char 
-                // val: freq of char 
-        
+        // tech: 
+            // anagrams - hash tables! 
+                // hash tables - count freq of chars 
+                // if each string has same hash table ....
+                // ... then dealing w/ valid anagram pair! 
+
         // time & space comp: 
             // time: o(n + m) 
-                // n & m - number of chars dictated by length of strings resp 
+                // bc we're iterating thr 2 strings
             // space: o(1) 
-                // 26 possible chars we can store --> simplifies to o(1)
-        
-        // 1. check edge case 
+                // constant - bc we know we're dealing w/ 26 chars 
+
+        // 1. EDGE CASE CHECK - if same length! 
         if(s.length() != t.length())
         {
-            return false;
+            return false; // not an anagram instantly :(
         }
 
-        // 2. otherwise, create hash tables to compare 
+        // 2. create hash tables 
         unordered_map<char, int> hashS; 
         unordered_map<char, int> hashT; 
 
+        // 3. populate hash tables 
         for(int i = 0; i < s.length(); i++)
         {
             hashS[s[i]]++; 
-            hashT[t[i]]++;
+            hashT[t[i]]++; 
         }
 
-        // 3. check for equivalency 
-            // checks if both hash tables share the same key-val pairs
+        // 4. return whether or not the 2 hash tables are equivalent 
+            // if yes... then we're dealing w/ valid anagrams!
+            // valid anagram pairs share the same key-val pairs 
+                // aka, each word is made up of the same chars! 
         return hashS == hashT; 
+
     }
 };
