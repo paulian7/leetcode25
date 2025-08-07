75f14c5551466cbfccbe618138fa9bcdb84b98b7
Paulian Le <paulianle7@gmail.com>
2025-08-07 00:00:44 -0700

    Time: 0 ms (100%), Space: 9.1 MB (11.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // tech: 
-            // hash table - will cross check to see if open closed correctly 
-            // stacks! 
-        
-        // time & space comp: 
-            // time: 
-            // space: 
-        
-        // 1. declare stack
-        stack<char> cStack;
+        // tech: stacks & hash table 
+            // RMBR - stacks are LIFO (last in, first out) 
 
-        // 2. declare hash table to work w/ 
+        // time & space comp: 
+            // time: o(n) 
+            // space: o(n) 
+        
+        // 1. declare stack 
+        stack<char> cStack; 
+
+        // 2. declare hash table to help track closed & open parentheses 
         unordered_map<char, char> closeToOpen = {
             {')', '('}, 
-            {'}', '{'},
-            {']', '['}
+            {']', '['}, 
+            {'}', '{'}, 
         };
 
-        // 3. iterate thr string 
-        for(int i = 0; i < s.length(); i++)
+        // 3. FINALLY iterate thr our string 
+        for(char c : s) 
         {
-            // check if dealing w/ closing paren 
-            if(closeToOpen.count(s[i]))
+            // check if dealing w/ CLOSING parentheses 
+                // count() - method that checks for existence of key in hash table 
+            if(closeToOpen.count(c))
             {
-                // dealing w/ closing! 
-                if(!cStack.empty() && cStack.top() == closeToOpen[s[i]])
+                // check if we've recently come across the OPENING paren. && is stack is NOT empty 
+                if(!cStack.empty() && cStack.top() == closeToOpen[c])
                 {
-                    cStack.pop(); // pop off open paren, has been prop closed!
+                    cStack.pop(); // pop open paren. from top of stack bc we've found a match
                 }
                 else 
                 {
-                    return false; // no proper closure
+                    return false; // no match found for closing paren. 
                 }
             }
             else 
             {
-                cStack.push(s[i]);
+                // dealing w/ open paren --> so add to stack to later maybe be popped off by closing paren
+                cStack.push(c); 
             }
         }
 
-
-
-        // 4. returns true if stack empty 
-            // empty means all open paren has closed!
+        // 4. if stack is empty --> we've closed everything --> return true :) 
         return cStack.empty(); 
     }
 };
