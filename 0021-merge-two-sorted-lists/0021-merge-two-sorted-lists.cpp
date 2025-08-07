cbd4c3e023bcce644e360b869c8becc8baa5388b
Paulian Le <paulianle7@gmail.com>
2025-08-07 00:08:35 -0700

    Time: 0 ms (100%), Space: 19.3 MB (98.91%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,58 +11,48 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // tech: iterative approach 
+        // tech: pointers 
 
         // time & space comp: 
-            // time: o(n + m), n and m being lengths of the 2 resp lists 
-            // space: o(1) 
-        
-        // 1. declare and initialize a dummy node 
-            // helps us avoid edge cases of an empty linked list 
-            // dummyNodePtr points to the dummy node we created w/ a val of -1 
-        ListNode *dummyNodePtr = new ListNode(-1); 
+            // time: o(n + m)
+            // space: o(1)
 
-        // have a tailPtr point to the last node in our linked list 
-            // which is the dummy node for now 
-        ListNode *tailPtr = dummyNodePtr; 
+        // 1. create a dummyPtr - helps us iterate thr our new merged list afterwards
+        ListNode *dummyPtr = new ListNode(-1);
 
-        // 2. iterate thr the 2 lists and create a new merged list of the 2 
-            // as long as both lists are nonempty 
-        while((list1 != nullptr) && (list2 != nullptr))
+        // 2. have a pointer point to last node of our new list for now 
+        ListNode *tailPtr = dummyPtr; 
+
+        // 3. start iterating thr the 2 lists
+        while((list1 != nullptr) && (list2 != nullptr)) 
         {
             if(list1 -> val < list2 -> val)
             {
-                // add l1 val to end of tail 
                 tailPtr -> next = list1; 
 
-                // update l1 ptr 
                 list1 = list1 -> next; 
             }
             else 
             {
-                // add l2 val to end of tail 
                 tailPtr -> next = list2; 
-
-                // update l2 ptr 
                 list2 = list2 -> next; 
             }
 
-            // always ensure you're updating tailPtr after new addition of a node 
+            // always update tailPtr to prep for next iteration 
             tailPtr = tailPtr -> next; 
         }
 
-        // 3. add any remaining nodes from the lists (if any) 
+        // 4. ensure we've iterated fully thr both lists
         if(list1 != nullptr)
         {
-            tailPtr -> next = list1; 
+            tailPtr -> next = list1;
         }
         else 
         {
             tailPtr -> next = list2; 
         }
 
-        // 4. return next node after the dummyNode 
-            // which gives us our new merged list 
-        return dummyNodePtr -> next; 
+        // 5. return new merged list
+        return dummyPtr -> next;
     }
 };
