676ac077c9df8351ae5282ffba219fee8bb62d08
Paulian Le <paulianle7@gmail.com>
2025-08-29 15:38:22 -0700

    Time: 12 ms (38.58%), Space: 11.8 MB (79.92%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,40 +9,31 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // tech: 2 pointers approach! 
-            // will have fast & slow ptr 
-                // fast: moves 2 steps at a time 
-                // slow: moves 1 step at a time 
-            // if no cycle: 
-                // fastPtr will eventually reach the end -> return false; 
-            // if cycle: 
-                // fastPtr will eventually meet up w/ slowPtr -> return true; 
-        
+        // tech: fast & slow ptrs
+
         // time & space comp: 
             // time: o(n) 
-            // space: o(1)
-        
-        // 1. declare & initalize our 2 ptrs (default to head first)
+            // space: o(1) 
+
+        // 1. declare & initialize fast & slow ptrs
         ListNode *fastPtr = head; 
         ListNode *slowPtr = head;
 
-        // 2. start our 2 ptrs strategy 
-            // continue while loopin' until: 
-                // fastPtr is valid && isn't the last node in the list
-        while(fastPtr && fastPtr -> next)
+        // 2. execute loop to identify whether or not cycle 
+            // if fastPtr == slowPtr --> dealing w/ cycle! 
+        while((fastPtr != nullptr) && (fastPtr -> next != nullptr))
         {
-            // update ptrs accordingly 
             slowPtr = slowPtr -> next; 
             fastPtr = fastPtr -> next -> next; 
 
-            // check for cycle now! 
+            // 3. check for cycle: 
             if(slowPtr == fastPtr)
             {
-                return true; // cycle exists
+                return true;
             }
         }
 
-        // 3. return false if fastPtr reaches the end
+        // 4. otherwise, return false as loop terminated w/o any == 
         return false;
     }
 };
