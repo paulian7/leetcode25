3deaff52751df9f51929d56eb061284911c1ff20
Paulian Le <paulianle7@gmail.com>
2025-09-04 14:49:29 -0700

    Time: 51 ms (57.51%), Space: 90.9 MB (46.83%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         // tech: hash sets 
-            // check for dups 
-            // insertion & searching for existence of element takes o(1) time 
-        
-        // time & space comp:
+
+        // time & space comp: 
             // time: o(n)
             // space: o(n)
-
-        // 1. create hash set 
+        
+        // 1. declare our hash set 
         unordered_set<int> hashSet; 
 
-        // 2. iterate thr array 
-        for(int i = 0; i < nums.size(); i++)
+        // 2. iterate thr given array 
+        for(int val : nums)
         {
-            // check whether or not this element already exists within the hash set 
-            if(hashSet.find(nums[i]) != hashSet.end())
+            // check for duplicates FIRST
+            if(hashSet.find(val) != hashSet.end())
             {
-                return true; // true -- as a dup has been found!
+                return true; 
             }
 
-            // else, haven't seen this element yet --> so add to hash set 
-            hashSet.insert(nums[i]); 
+            // otherwise, insert unseen val into hash set for later reference 
+            hashSet.insert(val); 
         }
 
-        // return false as no duplicates were found! 
         return false;
     }
 };
