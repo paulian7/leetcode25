7c5196a8da9626a83f66b85c826b30245955dd39
Paulian Le <paulianle7@gmail.com>
2025-09-06 14:44:53 -0700

    Time: 0 ms (100%), Space: 8.2 MB (47.86%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        // tech: modulo operator and shifting bits 
+        // tech: modulo operator & shifting bits 
 
         // time & space comp: o(1) 
 
-        // 1. declare var to return as result 
-        int result = 0;
+        // 1. declare result var to return 
+        int result = 0; 
 
-        // 2. iterate thr bits til all relvant bits are gone thr
+        // 2. iterate thr the bit representation of 'n' til no more relvant bits to 'n'
         while(n > 0)
         {
-            // 3. check if our curr bit is a 0 or 1 by using a modulo operator 
-                // if 1 --> 1 % 2 = 1 
-                // if 0 --> 0 % 2 = 0 
+            // mod operator curr bit to add +1 to result if we're curr at a 1
             result += n % 2; 
 
-            // 4. shift bits to the right now
-                // to look at next col (aka left from curr) 
+            // shift to next bit to look @ 
             n = n >> 1; 
         }
 
