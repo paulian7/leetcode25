042f8647687e33bc93291978d39596a387b2389a
Paulian Le <paulianle7@gmail.com>
2025-09-19 12:04:57 -0700

    Time: 3 ms (51.83%), Space: 10 MB (11.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,33 +2,35 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         // tech: 
-            // anagrams -- share same char freq 
-                // hash tables! - help us track char freq
-
+            // checking for anagrams 
+                // words that have same char freq as another word
+            // hash tables - account for char freq 
+        
         // time & space comp: 
-            // time: o(n + m)
-                // iterating thr both strings, s & t 
-            // space: o(1) 
-                // constant time, as we're just utilizing pointers
-                // also account for hash table (but limited to just 26 chars -> o(1))
+            // time: o(n + m) 
+                // bc iterating thr 2 strings respectively 
+            // space: 
+                // o(1) -- due to the fact that our characters we're dealing w/ are limited to the alphabet 
+                    // o(26) -> simplifies to o(1)
 
-        // 0. check if same length first 
+        // 1. check for length of the 2 strings 
         if(s.length() != t.length())
         {
-            return false;
+            return false; // need same length to be anagram :(
         }
 
-        // 1. declare hash tables
+        // 2. create hash tables to begin check
         unordered_map<char, int> hashS; 
-        unordered_map<char, int> hashT;
+        unordered_map<char, int> hashT; 
 
-        // 2. populate hash tables
+        // 3. populate hash tables 
         for(int i = 0; i < s.length(); i++)
         {
-            hashS[s[i]]++;
+            hashS[s[i]]++; 
             hashT[t[i]]++;
         }
 
+        // 4. check if both has tables line up 
         return hashS == hashT;
         
     }
