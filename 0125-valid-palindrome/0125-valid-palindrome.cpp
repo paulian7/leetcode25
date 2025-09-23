71b2479b56de33a21603fa720e7de69e3d1fea99
Paulian Le <paulianle7@gmail.com>
2025-09-23 11:07:42 -0700

    Time: 0 ms (100%), Space: 10 MB (41.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // tech: 
-            // palindrome - read same forwards and backwards
-            // 2 pointers method! 
-                // can compare start && end of string @ the same time 
-            
-        // time & space comp: 
-            // time: o(n) 
-                // going thr each char once -- no repeats
-            // space: o(1) 
-                // as we're just using pointers -- integer variables (nothing in comparison to hash table declarations)
+        // tech: palindrome?? 
+            // read same forward and backwards 
+            // want a way to compare both ends @ the same time 
+            // 2 pointers! 
         
-        // 1. declare & initialize 2 pointers 
+        // time & space comp: 
+            // time: o(n)
+            // space: o(1)
+        
+        // 1. declare our 2 pointers 
         int leftPtr = 0; 
         int rightPtr = s.length() - 1;
 
-        // 2. start 2 ptrs method 
-        while(leftPtr < rightPtr)
+        // 2. execute 2 pointers method -- iterate thr string --> time comp: o(n)
+        while(rightPtr > leftPtr)
         {
-            // update pointers accordingly until equivalence check! 
-            while(!isalnum(s[leftPtr]) && leftPtr < rightPtr)
+            // update pointers accordingly if NOT alphanumeric char
+            while(leftPtr < rightPtr && !isalnum(s[leftPtr])) // update left accord. 
             {
                 leftPtr++; 
             }
 
-            while(!isalnum(s[rightPtr]) && rightPtr > leftPtr)
+            while(rightPtr > leftPtr && !isalnum(s[rightPtr])) // update right accord.
             {
-                rightPtr--; 
+                rightPtr--;
             }
 
-            // NOW - check for equivalence
+            // check for equivalency 
             if(tolower(s[leftPtr]) != tolower(s[rightPtr]))
             {
-                return false; // not palindrome, return false right away
+                return false;
             }
 
-            // otherwise, if all pass, update pointers for next iteration
-            leftPtr++; 
+            // otherwise, update pointers
+            leftPtr++;
             rightPtr--;
         }
 
-        // 3. if while-loop finishes - palindrome is true :)
         return true;
     }
 };
