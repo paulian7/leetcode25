42a73de0503911de127bd100c3df670932ad53be
Paulian Le <paulianle7@gmail.com>
2025-09-23 14:39:09 -0700

    Time: 7 ms (6.43%), Space: 97.4 MB (25.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // ?: want the biggest difference 
-            // buy low, sell high
-        
-        // tech: 2 pointers 
-        
+        // tech: want maximum profit
+            // want BUY LOW, SELL HIGH --> 2 pointers 
+
         // time & space comp: 
             // time: o(n)
             // space: o(1) 
+                // using a constant number of integer variables -- no big extra data structures
+        
+        // 1. declare variable to return as final result
+        int maxProfit = 0;
 
-        // 1. declare variable to return as our final result 
-        int maxProfit = 0; 
-
-        // 2. execute 2 pointers method 
+        // 2. execute 2 ptrs method
         int leftPtr = 0; // buy 
         int rightPtr = 1; // sell 
 
-        // rightPtr is what will mainly be moving forward 
+        // 3. iterate thr
+            // rightPtr is acting as our iterator here 
         while(rightPtr < prices.size())
         {
-            // CONDITION - check if a profit can be made 
+            // CONDITION -- check. if a profit can be made 
             if(prices[rightPtr] > prices[leftPtr])
             {
                 int diff = prices[rightPtr] - prices[leftPtr]; 
 
-                maxProfit = max(maxProfit, diff); 
+                // update maxProfit appropriately 
+                maxProfit = max(maxProfit, diff);
             }
             else 
             {
-                // otherwise, need to update leftPtr bc found an even smaller val w/ rightPtr 
-                leftPtr = rightPtr; 
+                // need to update leftPtr bc found even smaller val in rightPtr
+                leftPtr = rightPtr;
             }
 
-            // always update rightPtr at the end to prepare for next iteration 
-            rightPtr++; 
+            // always ensure you update rightPtr to prepare for next iteration 
+            rightPtr++;
         }
 
-        return maxProfit; 
+        return maxProfit;
     }
 };
