502ee9a74491d8883fac3de62ff113251639ccdc
Paulian Le <paulianle7@gmail.com>
2025-09-23 17:03:01 -0700

    Time: 2 ms (13.43%), Space: 9.2 MB (13.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,48 +1,49 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // tech: stacks & hash table 
-            // RMBR - stacks are LIFO (last in, first out) 
-
+        // tech: stacks & hash table
+            // stacks -- to ensure paren. all closed correctly 
+            // hash tables -- helps us reference the corr matching pairs w/ one another
+        
         // time & space comp: 
-            // time: o(n) 
-            // space: o(n) 
+            // time:
+            // space: 
         
         // 1. declare stack 
-        stack<char> cStack; 
+        stack<char> parenStack;
 
-        // 2. declare hash table to help track closed & open parentheses 
+        // 2. declare hash table to be used as reference 
         unordered_map<char, char> closeToOpen = {
             {')', '('}, 
-            {']', '['}, 
             {'}', '{'}, 
-        };
+            {']', '['}, 
+        }; 
 
-        // 3. FINALLY iterate thr our string 
-        for(char c : s) 
+        // 3. iterate thr string 
+        for(char c : s)
         {
-            // check if dealing w/ CLOSING parentheses 
-                // count() - method that checks for existence of key in hash table 
-            if(closeToOpen.count(c))
+            // check what paren (close or open) we're dealing w/
+            if(closeToOpen.count(c)) // check for closing paren 
             {
-                // check if we've recently come across the OPENING paren. && is stack is NOT empty 
-                if(!cStack.empty() && cStack.top() == closeToOpen[c])
+                if(!parenStack.empty() && parenStack.top() == closeToOpen[c])
                 {
-                    cStack.pop(); // pop open paren. from top of stack bc we've found a match
+                    // pop open paren from top of stack
+                    // bc we've found a matching pair!
+                    parenStack.pop(); 
                 }
                 else 
                 {
-                    return false; // no match found for closing paren. 
+                    return false; // unsuccessful matching :(
                 }
             }
             else 
             {
-                // dealing w/ open paren --> so add to stack to later maybe be popped off by closing paren
-                cStack.push(c); 
+                // dealing w/ open paren --> so add to stack to later be popped
+                parenStack.push(c);
             }
         }
 
-        // 4. if stack is empty --> we've closed everything --> return true :) 
-        return cStack.empty(); 
+        // 4. IF stack is EMPTY --> we've closed everything, so return true! 
+        return parenStack.empty();
     }
 };
