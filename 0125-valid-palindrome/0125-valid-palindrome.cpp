9dc636c94516b057cb8cb9a07c2ebba16d5e3bb6
Paulian Le <paulianle7@gmail.com>
2025-09-25 07:05:18 -0700

    Time: 3 ms (31.73%), Space: 9.9 MB (57.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // tech: two pointers 
-            // let's us compare the front && end of string @ the same time 
+        // ?: what's a palindrome?... read same forwards and backwards
+            // want a way to compare both front && back @ the same time 
+            // !! --> 2 pointers method
         
-        // time & space comp: 
-            // time: o(n) 
-                // as we're iterating from both ends of the string, all elements iterated at least once
-            // space: o(1) 
-                // no other extra data structures are created, other than the 2 integer vars we declared & initialized
-                // would be a diff story if we declared hash tables 
-        
-        // 1. declare & initialize 2 pointers to respective ends of string 
-        int leftPtr = 0; // start of str
-        int rightPtr = s.length() - 1; // end of str 
+        // tech: 2 pointers method 
 
-        // 2. start iterating 
+        // time & space comp: 
+            // time: 
+            // space: 
+
+        // 1. declare our 2 pointers
+        int leftPtr = 0; // start 
+        int rightPtr = s.length() - 1; // end
+
+        // 2. start the process 
         while(leftPtr < rightPtr)
         {
-            // update pointers accordingly IF not alphanumeric
+            // update pointers FIRST if they're not alphanumeric -- as nonalphanumeric chars are also included here 
             while(leftPtr < rightPtr && !isalnum(s[leftPtr]))
             {
-                leftPtr++; // update left to be valid
+                leftPtr++; 
             }
 
             while(rightPtr > leftPtr && !isalnum(s[rightPtr]))
             {
-                rightPtr--; // update right to be valid
+                rightPtr--;
             }
 
-            // check for equivalence now 
-                // ensure that chars are LOWERCASED FIRST before checking!
+            // NOW -- check for == 
+                // ensure both chars are lowercased FIRST though before the check
             if(tolower(s[leftPtr]) != tolower(s[rightPtr]))
             {
-                return false; // return false immediately if not == 
+                return false; 
             }
 
-            // UPDATE POINTERS FOR NEXT ITERATION
+            // always ensure pointers are updated before next iteration 
             leftPtr++; 
             rightPtr--;
         }
 
-        // true -- if while-loop finishes execution 
+        // 3. once exit out of while loop, all checks passed --> dealing w/ palindrome :)
         return true;
     }
 };
