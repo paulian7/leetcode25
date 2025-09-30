cb7d2b91e2f46010c3b52a1551a082ce2a3e04f1
Paulian Le <paulianle7@gmail.com>
2025-09-30 12:37:59 -0700

    Time: 57 ms (29.61%), Space: 91 MB (31.29%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // tech: hash sets -- great for checking for existence --> takes o(1) 
-
-        // time & space comp: 
-            // time: o(n) -- just iterating thr each element within array one by one
-            // space: o(n) 
-                // in the case all elements are unique -> we end up storing n elements in hashSet 
-
+        // tech: detecting duplicates 
+            // hash sets! --> checking for existence takes o(1) time 
+        
+        // time & space comp:  
+            // time: o(n)
+            // space: o(n)
+        
         // 1. declare hash set 
-        unordered_set<int> hashSet; 
+            // acts as a reference -- holds UNIQUE values we've seen so far
+        unordered_set<int> seenVals;
 
         // 2. iterate thr array 
         for(int val : nums)
         {
-            // check for duplicates first right away 
-            if(hashSet.find(val) != hashSet.end())
+            // check immediately first if we see a duplicate?? 
+            if(seenVals.find(val) != seenVals.end())
             {
-                // oh no duplicate found! 
-                return true; 
+                // if-condition true? --> duplicate has been found! 
+                return true;
             }
 
-            // else, insert to hashSet for later checking 
-            hashSet.insert(val); 
+            // else, no duplicate?? add unique val to hashSet for later reference 
+            seenVals.insert(val); 
         }
 
-        // all checks passed ocne exit out of for-each loop 
-        return false; // no duplicates :) 
+        // 3. return true once we've iterated thr entire array w/o any dups detected
+        return false;
     }
 };
