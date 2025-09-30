e89f30a3e753fec077a3a4e4fd8360f0db68a97a
Paulian Le <paulianle7@gmail.com>
2025-09-30 13:25:46 -0700

    Time: 75 ms (8.25%), Space: 34.3 MB (10.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // tech: 
-            // hashing -- hash table (freq string->strs that share it)
+        // tech: group anagrams together? 
+            // hash tables! -- key: freq string, val: strings that share same freq string
 
         // time & space comp: 
             // time: o(n * m)
-            // space: o(n) -- 'n' being num of strings stored into hash table
-        
-        // 1. declare hash table 
-        unordered_map<string, vector<string>> hashTab;
+            // space:
+                // o(n) - for the number of strings stored in the hash table created
+                // o(n * m) for our output list 
+                    // n — num of strings; m — length of longest string 
 
-        // 2. iterate thr string 
+        // 1. declare hash table 
+        unordered_map<string, vector<string>> hashTable;
+
+        // 2. iterate thr array
         for(int i = 0; i < strs.size(); i++)
         {
-            // iterate thr each char in each element str
-            vector<int> count(26, 0); // have way to track char freqs
+            // want to iterate thr each char within the curr string now 
+            vector<int> count(26, 0); 
             for(char c : strs[i])
             {
-                // utilize an ascii trick here 
-                    // curr - 'a' --> gives corr index to increment 
-                count[c - 'a']++; 
+                count[c - 'a']++;
             }
 
-            // convert "count" array to a string to use as our "key" for hash table
+            // convert count array into a string to be used as a key 
             string key; 
             for(int i = 0; i < 26; i++)
             {
                 key += to_string(count[i]) + ","; 
             }
 
-            // add key-val pair to hash table 
-            hashTab[key].push_back(strs[i]);
+            // officially add as a key-val pair to hash table
+            hashTable[key].push_back(strs[i]);
         }
 
-        // 3. return only the pair.second now 
+        // 3. return JUST the grouped anagrams
         vector<vector<string>> resAnagrams;
-        for(auto const& pair : hashTab)
+        for(auto const& pair : hashTable)
         {
             resAnagrams.push_back(pair.second); 
         }
