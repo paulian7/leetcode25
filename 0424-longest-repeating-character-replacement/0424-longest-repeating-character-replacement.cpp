6a289a46fcfe58f20585300db2686e28429d8996
Paulian Le <paulianle7@gmail.com>
2025-10-02 12:46:27 -0700

    Time: 7 ms (44.76%), Space: 11 MB (18.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,52 +1,43 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // tech: hashing & sliding window 
-            // "longest substring" --> SLIDING WINDOW
-            // "containing same letter" --> us dealing w/ frequency --> hash tables
+        // tech: substring --> sliding window
+            // containing same letter --> dealing w/ freq here --> hash table
         
         // time & space comp: 
-            // time: o(n) -- iterating thr str 
-            // space: o(m) -- num of unique chars stored within our hash table
-        
-        // 1. declare final ans to return 
-        int longest = 0; 
+            // time: o(n)
+            // space: o(n)
 
-        // 2. have a helper variable 
-            // helps track the highest freq we've seen so far 
-            // allows us to have a time comp of o(n) 
-                // as we're not constantly checking again and again for the highest freq in the table
-        int maxFreq = 0; 
+        // 1. declare var to return as result 
+        int longest = 0;
 
-        // 3. declare hash table to track freq of each unique val
-        unordered_map<char, int> hashCount;
+        // 2. declare hash table to track freq 
+        unordered_map<char, int> hashTable;
 
-        // 4. FINALLY execute the sliding window technique 
+        // 3. have a variable to track largest freq 
+            // helps us do everything in one-pass 
+            // don't have to constantly check for largest frequency 
+        int maxFreq = 0;
+
+        // 4. iterate thr string now --> establishing sliding window tech
         int leftPtr = 0; 
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // update hash table trackin' chars freqs
-            hashCount[s[rightPtr]]++; 
+            // populate hash table first 
+            hashTable[s[rightPtr]]++;
 
-            // update maxFreq accordingly if we've found an even larger char freq
-            maxFreq = max(hashCount[s[rightPtr]], maxFreq);
+            // update maxFreq accordingly 
+            maxFreq = max(hashTable[s[rightPtr]], maxFreq);
 
-            // check if window is needed to be updated
+            // check if window update is needed? 
             while((rightPtr - leftPtr + 1) - maxFreq > k)
             {
-                // NEED TO UPDATE WINDOW... 
-                    // num of replacements needed... given by --> (rightPtr - leftPtr + 1) - maxFreq 
-                    // ... is greater than what's given by "k"
+                hashTable[s[leftPtr]]--; 
 
-                // update freq count in leftPtr in hash table
-                hashCount[s[leftPtr]]--;
-
-                // officially shrink the sliding window thr incrementing leftPtr
                 leftPtr++;
             }
 
-            // update final res before next iteration 
-            longest = max((rightPtr - leftPtr + 1), longest);
+            longest = max(longest, (rightPtr - leftPtr + 1));
         }
 
         return longest;
