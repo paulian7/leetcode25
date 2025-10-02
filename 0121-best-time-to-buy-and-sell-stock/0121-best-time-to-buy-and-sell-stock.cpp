29540aa3b3f512c2f49f89738ebc28f62131b3db
Paulian Le <paulianle7@gmail.com>
2025-10-02 12:16:14 -0700

    Time: 1 ms (36.1%), Space: 97.4 MB (25.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // tech: 
-            // want to find largest difference... but in one-pass! 
-            // 2 pointers 
+        // tech: want to find largest diff 
+            // buy low, sell high --> use 2 pointers!~ 
         
         // time & space comp: 
             // time: o(n)
             // space: o(1)
 
-        // 1. declare variable to return as result 
+        // 1. declare var to return as final result 
         int maxProfit = 0; 
 
-        // 2. declare 2 pointers 
-        int leftPtr = 0; // buy 
-        int rightPtr = 1; // sell 
+        // 2. execute 2 pointers method
+        int buyPtr = 0; // when we buy
+        int sellPtr = 1; // when se sell 
 
-        // 3. iterate thr array of prices now 
-        while(rightPtr < prices.size())
+        // 3. start iterating thr
+            // rightPtr acts like our "iterator"
+        while(sellPtr < prices.size())
         {
-            // check if any profit?? 
-            if(prices[rightPtr] > prices[leftPtr])
+            // CONDITION -- check if a profit can be made 
+            if(prices[sellPtr] > prices[buyPtr])
             {
-                int currDiff = prices[rightPtr] - prices[leftPtr]; 
-                maxProfit = max(currDiff, maxProfit); 
+                // check currProfit 
+                int diff = prices[sellPtr] - prices[buyPtr]; 
+
+                // update maxProfit accordingly 
+                maxProfit = max(diff, maxProfit); 
             }
             else 
             {
-                leftPtr = rightPtr; 
+                // otherwise, we found an even smaller val in rightPtr
+                    // need to swap the 2 vals
+                buyPtr = sellPtr;
             }
 
-            // always update rightPtr for the next iteration 
-            rightPtr++;
+            // always update rightPtr to prep for next iteration 
+            sellPtr++;
         }
 
-        return maxProfit;
+        return maxProfit; 
     }
 };
