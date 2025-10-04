db229ddd7e778d68e6e7e0201fb61063ef651deb
Paulian Le <paulianle7@gmail.com>
2025-10-03 23:56:10 -0700

    Time: 0 ms (100%), Space: 20 MB (8.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // tech: return k most frequent elements 
-            // dealing w/ frequency --> hash tables! 
-        
+        // tech: 
+            // hash table - stores key-val pairs
+                // key: val; value: freq of that val
+            // bucket sort - indices represent freq of the value stored @ that index
+
         // time & space comp: 
             // time: o(n)
             // space: o(n)
 
         // 1. declare hash table 
-        unordered_map<int, int> hashTable; 
-
-        // 2. iterate thr array, populating hashTable 
+        unordered_map<int, int> hashCount; 
+        
+        // 2. populate hash table 
         for(int val : nums)
         {
-            hashTable[val]++; 
+            hashCount[val]++;
         }
 
-        // 3. sort numbers into their appropriate frequency bucket 
-            // "freq bucket" - being the index 
-        vector<vector<int>> count(nums.size() + 1); 
-        for(auto const& pair : hashTable)
+        // 3. sort into respective buckets 
+        vector<vector<int>> freqCount(nums.size() + 1); 
+        for(auto const& pair : hashCount)
         {
-            count[pair.second].push_back(pair.first); 
+            freqCount[pair.second].push_back(pair.first);
         }
 
-        // 4. return top k freq elements now 
-        vector<int> result; 
-        for(int i = count.size() - 1; i > 0; i--)
+        // 4. return top k elements now 
+        vector<int> result;
+        for(int i = freqCount.size() - 1; i > 0; i--)
         {
-            // ensure you're iterating thr each val within each bucket 
-            for(int val : count[i])
+            for(int val : freqCount[i])
             {
-                result.push_back(val); 
+                result.push_back(val);
+
                 if(result.size() == k)
                 {
                     return result;
