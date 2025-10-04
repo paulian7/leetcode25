21bd8272cfd1fe998094a21ae0a84437c8124ee9
Paulian Le <paulianle7@gmail.com>
2025-10-04 00:00:02 -0700

    Time: 2 ms (57.02%), Space: 20 MB (8.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // tech: 
-            // hash table - stores key-val pairs
-                // key: val; value: freq of that val
-            // bucket sort - indices represent freq of the value stored @ that index
-
+        // tech: want to return MOST FREQUENT ELEMENTS 
+            // dealing w/ frequency --> hash tables 
+        
         // time & space comp: 
             // time: o(n)
             // space: o(n)
-
-        // 1. declare hash table 
-        unordered_map<int, int> hashCount; 
         
-        // 2. populate hash table 
+        // 1. declare hash table 
+        unordered_map<int, int> hashTab; 
+
+        // 2. iterate thr array, populating hash table
         for(int val : nums)
         {
-            hashCount[val]++;
+            hashTab[val]++;
         }
 
-        // 3. sort into respective buckets 
-        vector<vector<int>> freqCount(nums.size() + 1); 
-        for(auto const& pair : hashCount)
+        // 3. sort into buckets
+            // indices represent the freq of that value
+        vector<vector<int>> count(nums.size() + 1);
+        for(auto const& pair : hashTab)
         {
-            freqCount[pair.second].push_back(pair.first);
+            count[pair.second].push_back(pair.first); 
         }
 
-        // 4. return top k elements now 
+        // 4. return top k now 
         vector<int> result;
-        for(int i = freqCount.size() - 1; i > 0; i--)
+        for(int i = count.size() - 1; i > 0; i--)
         {
-            for(int val : freqCount[i])
+            for(int val : count[i])
             {
-                result.push_back(val);
-
+                result.push_back(val); 
                 if(result.size() == k)
                 {
                     return result;
