8734d45b8dd80d7498acd76e40822ff995c47dd1
Paulian Le <paulianle7@gmail.com>
2025-10-06 22:46:25 -0700

    Time: 0 ms (100%), Space: 19.9 MB (13.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // tech: want to return MOST FREQUENT ELEMENTS 
-            // dealing w/ frequency --> hash tables 
+        // tech: 
+            // dealing w/ frequency of elements --> hash tables
         
         // time & space comp: 
             // time: o(n)
             // space: o(n)
         
         // 1. declare hash table 
-        unordered_map<int, int> hashTab; 
+        unordered_map<int, int> hashTable;
 
-        // 2. iterate thr array, populating hash table
+        // 2. populate hash table
         for(int val : nums)
         {
-            hashTab[val]++;
+            hashTable[val]++;
         }
 
-        // 3. sort into buckets
-            // indices represent the freq of that value
-        vector<vector<int>> count(nums.size() + 1);
-        for(auto const& pair : hashTab)
+        // 3. sort vals into buckets (indices) that rep their correspond freq
+        vector<vector<int>> freqCount(nums.size() + 1); 
+        for(auto const& pair : hashTable)
         {
-            count[pair.second].push_back(pair.first); 
+            freqCount[pair.second].push_back(pair.first);
         }
 
-        // 4. return top k now 
+        // 4. return top k freq elements 
         vector<int> result;
-        for(int i = count.size() - 1; i > 0; i--)
+        for(int i = freqCount.size() - 1; i > 0; i--)
         {
-            for(int val : count[i])
+            for(int val : freqCount[i])
             {
                 result.push_back(val); 
                 if(result.size() == k)
