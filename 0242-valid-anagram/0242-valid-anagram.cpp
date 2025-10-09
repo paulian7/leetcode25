1c01cf7a6bed1aef2d070ab72c0153e8da02a3c3
Paulian Le <paulianle7@gmail.com>
2025-10-09 16:24:13 -0700

    Time: 3 ms (51.38%), Space: 10 MB (11.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,35 +2,31 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         // tech: 
-            // anagrams - both strings are made up of the same characters 
-            // hash tables perfect to use! 
-                // can store key-value pairs for us 
-                    // key: characters 
-                    // val: freq of that resp character
+            // anagram - sharing same char frequency 
+            // frequency --> hash tables
         
         // time & space comp: 
-            // time: o(n + m)
+            // time: o(n + m) 
             // space: o(1)
         
-        // 1. edge case - checking for length
+        // 1. EDGE CASE - check for same length 
         if(s.length() != t.length())
         {
             return false;
         }
 
-        // 2. declare hash tables to rep each resp string
+        // 2. build hash tables for each resp string 
         unordered_map<char, int> hashS; 
-        unordered_map<char, int> hashT;
+        unordered_map<char, int> hashT; 
 
-        // 3. populate hash tables 
         for(int i = 0; i < s.length(); i++)
         {
             hashS[s[i]]++; 
-            hashT[t[i]]++;
+            hashT[t[i]]++; 
         }
 
-        // 4. compare the 2 hash tables if they're equivalent now
-            // yes? -- both are anagrams of one another :)
-        return hashS == hashT;
+        // 3. check equivalence of hash tables 
+            // if both share same key-val pairs --> dealing w/ anagrams! :) 
+        return hashS == hashT; 
     }
 };
