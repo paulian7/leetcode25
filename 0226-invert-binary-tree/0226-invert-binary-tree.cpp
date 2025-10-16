17167f176931f88b3d2e0e444154977238f012f7
Paulian Le <paulianle7@gmail.com>
2025-10-16 11:35:22 -0700

    Time: 0 ms (100%), Space: 12.5 MB (59.37%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -13,27 +13,28 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         // tech: recursion w/ the basis of DFS 
-            // DFS - depth first search 
-                // traverse thr left THEN right subtree(s)
-        
+            // DFS - depth first search... 
+                // where we traverse thr left THEN right subtree(s) 
+
         // time & space comp: 
             // time: o(n)
-            // space: o(n) 
+            // space: o(n) -- for recursion stack
         
         // 1. create base case for recursion 
-        if(!root)
+        if(!root) 
         {
-            return nullptr; 
+            return nullptr;
         }
 
-        // 2. perform swap operation on our 2 children of our curr root node
+        // 2. otherwise, perform swap op (takes o(1) time) on first set of children
         swap(root -> left, root -> right); 
 
-        // 3. perform recursive calls on each subtree (left / right) 
+        // 3. NOW, perform recursive calls on each subtree (left && right)
+            // DFS - rmbr goes thr left, then right subtree 
         invertTree(root -> left); 
-        invertTree(root -> right); 
+        invertTree(root -> right);
 
-        // 4. return the root of the newly inverted tree
-        return root; 
+        // 4. return new inverted tree by returning root 
+        return root;
     }
 };
