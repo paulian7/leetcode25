b940da641703b8dfee37f443a3e43986d73fb57c
Paulian Le <paulianle7@gmail.com>
2025-11-11 10:08:31 -0800

    Time: 0 ms (100%), Space: 14.2 MB (12.27%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,53 +2,57 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         // tech: 
-            // binary search! -- want time comp of o(logn), array sorted (but rotated))
+            // binary search: sorted, rotated, logn time comp
+            // will need to identify a pivot
         
         // time & space comp: 
             // time: o(logn) 
-            // space: o(1) 
+                // typical time comp of binary search
+            // space: o(1)
         
-        // 1. declare & initialize our result var to return 
-            // have smallest val be first element in arr for now 
+        // 1. declare & initialize our result var to return @ the end 
+            // default it to the first val in array for now
         int minVal = nums[0];
 
-        // 2. declare & initialize 2 pointers for BINARY SEARCH
+        // 2. declare & initialize 2 pointers we're working w/ for binary search
         int leftPtr = 0; 
         int rightPtr = nums.size() - 1;
 
-        // 3. BEGIN BINARY SEARCH ==
+        // 3. perform binary search 
         while(leftPtr <= rightPtr)
         {
-            // CASE1 == CHECK IF SUBARRAY SORTED 
+            // CASE 1 == CHECK IF SUBARRAY SORTED -- our current search space
             if(nums[leftPtr] < nums[rightPtr])
             {
-                minVal = min(minVal, nums[leftPtr]);
-                break; // BREAKS OUT OF WHILE-LOOP ENTIRELY
+                minVal = min(minVal, nums[leftPtr]); 
+                break; // break statement -- breaks out of nearest enclosing loop (aka our while-loop)
             }
 
-            // CASE 2 == WHEN ARR HAS BEEN ROTATED...
-                // need to find midPt to start performing biinary search
+            // CASE 2 == WHEN ARR IS ROTATED, dealing w/ strange order
+                // need to find midPt to start performing binary search
             int midPt = leftPtr + (rightPtr - leftPtr) / 2;
 
-            // check if midPt could poss be smallestVal 
+            // check if midPt could poss be smallest val
             minVal = min(minVal, nums[midPt]);
 
-            // decide to search in left or right section 
+            // decide to search in left or right section / side 
             if(nums[midPt] >= nums[leftPtr])
             {
-                // then... midPt part of left sorted portion (big vals)
+                // then.. midPt part of left sorted portion (the big vals).. bc everything's increasin' here
                 // so want to search right portion instead 
-                    // bc they should contain the smaller vals
-                // update leftPtr to search RIGHT HALF now
-                leftPtr = midPt + 1; 
+                    // bc it should contain the smaller vals
+                // update leftPtr to search RIGHT HALF now 
+                leftPtr = midPt + 1;
             }
             else 
             {
-                // want to search left portion 
-                rightPtr = midPt - 1;
+                // want to search LEFT PORTION
+                rightPtr = midPt - 1; 
             }
         }
 
+        // 4. return final result
         return minVal;
+        
     }
 };
