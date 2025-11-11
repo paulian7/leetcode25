c7e0d056348a4c1489f248ffae4505568351723f
Paulian Le <paulianle7@gmail.com>
2025-11-11 12:10:12 -0800

    Time: 0 ms (100%), Space: 15.2 MB (32.32%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,68 +2,68 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         // tech: 
-            // binary search 
-                // sorted array! 
-                // rotated array :(
-                // want search done in o(logn) 
+            // array sorted --> binary search
+            // sorted, but could be dealing w/ a rotated array lol
+            // want to return index of value specified by target 
+                // return -1 if can't find 
         
         // time & space comp: 
-            // time: o(logn) 
-                // search space is halved each iteration
+            // time: o(logn)
+                // BINARY SEARCH -- way to go if want time comp < o(n)
             // space: o(1) 
         
-        // 1. create 2 pointers for binary search
-        int leftPtr = 0; // low
-        int rightPtr = nums.size() - 1; // high
+        // 1. create our pointers 
+        int leftPtr = 0; // low 
+        int rightPtr = nums.size() - 1; // high 
 
-        // 2. perform search
+        // 2. iterate thr array
         while(leftPtr <= rightPtr)
         {
-            // calc midPt to help decide search space later 
+            // calc midPt to halve our search space (since we're doing binary search)
             int midPt = leftPtr + (rightPtr - leftPtr) / 2;
 
-            // CASE 1 == check if midPt is our target val
-            if(nums[midPt] == target)
+            // CASE 1 == CHECK IF OUR MIDPT IS OUR TARGET VAL
+            if(target == nums[midPt])
             {
-                return midPt; // return midPt's index --> as it points to target
-            } 
+                return midPt; // return midPt's index :)
+            }
 
-            // CASE 2 == figure out which half to search to find target
+            // CASE 2 - figure out which halve to search for target (left or right)
             if(nums[leftPtr] <= nums[midPt])
             {
-                // --- midPt val belongs to LEFT SORTED portion ---
-                // so, focus on left portion here 
+                // midPt val belongs to LEFT sorted portion.... 
+                // so, focus on left portion here
                 if(target > nums[midPt] || target < nums[leftPtr])
                 {
-                    // need to switch portions now - bc this one is invalid
-                        // switch to right portion!
-                    leftPtr = midPt + 1; 
+                    // need to switch to right portion here...
+                    leftPtr = midPt + 1;
                 }
                 else 
                 {
-                    // can stay in left portion - 
-                    // need to update rightPtr then to left half!...
-                        // which narrows our search space down further 
+                    // can stay in left portion!! 
+                    // need to update rightPtr then to left halve 
+                        // this narrows down our search space further
                     rightPtr = midPt - 1;
                 }
             }
             else 
             {
-                // --- midPt val belongs to RIGHT SORTED portion ---
+                // midPt val belongs to RIGHT sorted portoin
                 if(target < nums[midPt] || target > nums[rightPtr])
                 {
-                    // switch to left portion... 
+                    // switch to left portion...
                     rightPtr = midPt - 1;
                 }
                 else 
                 {
-                    // otherwise, stay in right portion 
+                    // can stay in right portion!
                     leftPtr = midPt + 1;
                 }
             }
+
         }
 
         // worst case, return -1 
-        return -1;
+        return -1; 
     }
 };
