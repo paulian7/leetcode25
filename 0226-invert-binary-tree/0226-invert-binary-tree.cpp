7741ee490f690439623ac38502f63fb59696dbe0
Paulian Le <paulianle7@gmail.com>
2025-11-12 20:36:52 -0800

    Time: 0 ms (100%), Space: 12.3 MB (85.68%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,28 +12,32 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        // tech: recursive depth first search (dfs) 
-            // traversing thr left -> then right subtree(s)
-
-        // time & space comp: 
-            // time: o(n), iterating thr each node
-            // space: o(n), unbalanced tree 
-
-        // 1. declare base case as always for recursion
+        // tech: recursive depth first search (dfs)
+            // where you traverse thr all the left subtrees first -> then right substrees
+        
+        // time & space comp:
+            // time: o(n), iterating thr each node 
+            // space: o(n), in the case we're dealing w/ an unbalanced tree 
+                // where all nodes happen to fall in the left substree only - for example
+                // tree being left / right skewed 
+        
+        // 1. declare base case as always w/ recursion 
+            // check to see if we have a tree to iterate thr
         if(!root)
         {
             return nullptr;
         }
 
-        // 2. otherwise, perform swap operation on root's children
-            // swap() takes o(1) time
-        swap(root -> left, root -> right);
+        // 2. otherwise, perform swap operation on curr root's children 
+            // swap() -- takes o(1) time 
+        swap(root -> left, root -> right); 
 
-        // 3. perform recursive calls on each subtree respectively now 
+        // 3. perform recursive calls on each substree respectively
+            // go left substree -> then right subtree
         invertTree(root -> left); 
         invertTree(root -> right);
 
-        // 4. return new inverted tree by returning the root 
+        // 4. return new inverted tree now by returning the root 
         return root;
     }
 };
