27c71ee33dee20c9f88a079b79cf2660165bfbf0
Paulian Le <paulianle7@gmail.com>
2025-11-12 23:03:53 -0800

    Time: 0 ms (100%), Space: 28.9 MB (72.2%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -13,54 +13,54 @@ class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         // tech: recursive depth first search (dfs) 
-
-        // time & space comp: 
-            // time: o(m * n)
-            // space: o(m + n)
-                // m - num of nodes in subRoot 
-                // n - num of nodes in root 
         
-        // 1. deal w/ edge cases 
-        // == when subRoot is empty --> technically subtree yes
+        // time & space comp: 
+            // time: o(m * n) 
+            // space: o(m + n)
+                // m - num of nodes in subroot 
+                // n - num of nodes in root 
+
+        // 1. cover base cases 
+            // when subroot is an empty tree --> so technically is a subroot of main tree
         if(!subRoot)
         {
             return true;
         }
 
-        // == when root is empty --> no
+        // when main tree is empty --> no
         if(!root)
         {
             return false;
         }
 
-        // 2. check if root's subtree = subRoot 
+        // 2. check if root right off the bat matches up w/ the subtree
         if(sameTree(root, subRoot))
         {
             return true;
         }
 
-        // 3. else, check for subRoot in either left or right subtree of root 
+        // 3. else, go search for "subRoot" in either left or right subtree of our main tree
         return isSubtree(root -> left, subRoot) || 
                 isSubtree(root -> right, subRoot); 
     }
 
-    // helper function 
+    // HELPER FUNCTION == 
     bool sameTree(TreeNode* root, TreeNode* subRoot) {
-        // dealing w/ empty trees -> subtrees yes
+        // edge case -- dealing w/ empy trees --> ofc same 
         if(!root && !subRoot)
         {
             return true;
         }
 
-        // when both trees aren't empty && vals of both are the same
-        if(root && subRoot && root -> val == subRoot -> val)
+        // edge case - when both trees aren't empty... check if values match up
+        if((root && subRoot) && (root -> val == subRoot -> val))
         {
-            // continue comparing the rest of the subtrees now if they're ==
+            // compare left && right subtrees now of root && subroot 
             return sameTree(root -> left, subRoot -> left) && 
                     sameTree(root -> right, subRoot -> right);
         }
 
-        // when one of the trees are nonempty
+        // else, return false.. like when one of the trees are nonempty 
         return false;
     }
 };
