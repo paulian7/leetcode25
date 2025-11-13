cbf73fada8488bd5d77fe262e278044b9e0ba373
Paulian Le <paulianle7@gmail.com>
2025-11-13 11:05:13 -0800

    Time: 0 ms (100%), Space: 12.4 MB (59.12%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,32 +12,30 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        // tech: recursive depth first search (dfs)
-            // where you traverse thr all the left subtrees first -> then right substrees
+        // tech: recursive depth first search (dfs) 
+            // going thr left subtree(s) first then right subtree(s) 
         
-        // time & space comp:
-            // time: o(n), iterating thr each node 
-            // space: o(n), in the case we're dealing w/ an unbalanced tree 
-                // where all nodes happen to fall in the left substree only - for example
-                // tree being left / right skewed 
+        // time & space comp: 
+            // time: o(n) 
+                // iterating node by node 
+            // space: o(n) 
+                // o(n), worst case - unbalanced tree 
+                // o(logn), best case - balanced tree 
         
-        // 1. declare base case as always w/ recursion 
-            // check to see if we have a tree to iterate thr
+        // 1. establish a base case - as you would w/ any recursive technique 
         if(!root)
         {
             return nullptr;
         }
 
-        // 2. otherwise, perform swap operation on curr root's children 
-            // swap() -- takes o(1) time 
-        swap(root -> left, root -> right); 
+        // 2. op - start swapping 
+        swap(root -> left, root -> right);
 
-        // 3. perform recursive calls on each substree respectively
-            // go left substree -> then right subtree
+        // 3. split overall problem - into subproblems -> just the left and right subtree(s)
         invertTree(root -> left); 
-        invertTree(root -> right);
+        invertTree(root -> right); 
 
-        // 4. return new inverted tree now by returning the root 
+        // 4. return completed invert tree 
         return root;
     }
 };
