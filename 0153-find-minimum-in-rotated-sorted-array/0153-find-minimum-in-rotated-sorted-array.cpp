628f059c7b7c07d36e13394688b7de035779d438
Paulian Le <paulianle7@gmail.com>
2025-11-13 12:54:03 -0800

    Time: 0 ms (100%), Space: 14.3 MB (12.37%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -2,55 +2,46 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         // tech: 
-            // sorted, rotated, time comp of o(logn) desired --> binary search
+            // binary search - rotated, sorted, time comp of logn
         
         // time & space comp: 
-            // time: o(logn) 
-                // using binary search 
+            // time: o(logn)
+                // w/ each iteration - the search space is HALVED
             // space: o(1) 
-                // in-place search algorithm
-
-        // 1. declare result to return as final answer 
-            // set to first val in array so far
+        
+        // 1. declare result to return final answer
         int minVal = nums[0]; 
 
-        // 2. set up 2 pointers used for binary search
+        // 2. set up pointers for binary search
         int leftPtr = 0; 
         int rightPtr = nums.size() - 1; 
 
-        // 3. start our search
-        while(leftPtr <= rightPtr) 
+        // 3. execute binary search 
+        while(leftPtr <= rightPtr)
         {
-            // check for condition -- of whether or not we're already in a sorted search space 
+            // check case -
             if(nums[leftPtr] < nums[rightPtr])
             {
-                minVal = min(minVal, nums[leftPtr]); 
+                minVal = min(minVal, nums[leftPtr]);
                 break;
             }
 
-            // otherwise -- start steps of binary search 
-            // calculate our midPoint -- what we use to help us decide which half to search
-                // by doing it this way, it helps us avoid any integer overflow 
-                // is still the same as (leftPtr + rightPtr) / 2 -- but this way helps prevent overflow
+            // else, need to find where to search 
+                // ensure that there's no integer overflow by doing it this way 
             int midPt = leftPtr + (rightPtr - leftPtr) / 2;
-
-            // check if midPt can poss be minVal 
-            minVal = min(minVal, nums[midPt]); 
+            minVal = min(minVal, nums[midPt]);
             
-            // decide which half to search 
             if(nums[leftPtr] <= nums[midPt])
             {
-                // midPt apart of increasing section -- want to swap to other section 
+                // the increasing section --> want to swap to small section 
                 leftPtr = midPt + 1; 
             }
             else 
             {
-                // want to search left -> to see if there's any smaller values 
                 rightPtr = midPt - 1; 
             }
         }
 
-        // if no min val can be found 
         return minVal;
     }
 };
