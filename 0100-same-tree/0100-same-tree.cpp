14789f96fd7c37fdd82815e6035a95557c9bff54
Paulian Le <paulianle7@gmail.com>
2025-11-12 22:48:54 -0800

    Time: 0 ms (100%), Space: 12.9 MB (14.71%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,31 +12,30 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // tech: recursive depth first search
-            // need same VALUES && STRUCTURE
-            // utilize depth first search as its handy w/ recursion 
+        // tech: recursive depth first search (dfs) 
+            // need same vals && structure
         
         // time & space comp: 
-            // time: o(p + q)
-                // p - num of nodes in tree 'p' 
-                // q - num of nodes in tree 'q' 
-            // space: 
-                // o(n) -- worst case, dealing w/ unbalanced trees 
-                // o(logn) -- best case, dealing w/ balanced tree 
+            // time: o(p + q) 
+                // p - num of nodes in 'p' tree 
+                // q - num of nodes in 'q' tree 
+            // space: o(n) 
+                // o(n) - unbalanced tree 
+                // o(logn) - best case, balanced tree
         
         // 1. start w/ base case as always w/ recursion 
-            // base case - empty trees - so techically equal 
-        if(!p && !q) 
+            // base case - empty trees = tech equal :) 
+        if(!p && !q)
         {
             return true;
         }
 
-        // 2. else, split problem into subproblems 
+        // 2. else, split problem into subproblems
         if((p && q) && (p -> val == q -> val))
         {
-            // perform recursive calls on: 
-                // left subtree of p && q 
-                // right subtree of p & q 
+            // perform recursive call!...
+                // on left subtree of p && q 
+                // then on right subtree of p && q 
             return isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right);
         }
         else 
