3f8c199079a49539c736068639c367c6b876e717
Paulian Le <paulianle7@gmail.com>
2025-11-13 13:51:26 -0800

    Time: 18 ms (31.54%), Space: 14.3 MB (21.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // tech -- hash sets & sliding window 
-            // "longest substring" - sliding window 
-            // "w/o repeating chars" - hash set 
-                // checking for existence - takes o(1) time...
-                // ...in comparison to arrays taking o(n)
-        
-        // time & space comp: 
-            // time: o(n) - n, length of str 
-                // iterating thr char by char
-            // space: o(m) 
-                // m - num of unique chars stored in hashSet
-        
-        // 1. declare final var to return 
-        int longest = 0; 
-
-        // 2. declare hash set to track seen chars that are unique 
         unordered_set<char> seenChars; 
 
-        // 3. execute sliding window technique 
-        int leftPtr = 0; 
+        int longest = 0;
+
+        int leftPtr = 0;
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // check if we need to shrink the window first 
-                // as in, check if we see a repeat w/ this curr char
+            // check for valid window
             while(seenChars.find(s[rightPtr]) != seenChars.end())
             {
-                // shrink window until condition no longer holds true
-
-                // remove repeat from hash set
                 seenChars.erase(s[leftPtr]); 
-                // update leftPtr now to officially shrink the window
-                leftPtr++; 
+                leftPtr++;
             }
 
-            // IF unique char -- add to hash set for later reference
+            // else, add to hash set 
             seenChars.insert(s[rightPtr]); 
-
-            // update final result now accordingly 
-            longest = max((rightPtr - leftPtr + 1), longest);
+            // update length
+            longest = max(longest, (rightPtr - leftPtr + 1));
         }
 
         return longest;
