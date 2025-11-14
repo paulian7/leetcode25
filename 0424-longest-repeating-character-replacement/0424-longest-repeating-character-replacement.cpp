23564ab37ce2c9da271ffca619232ad99a96097d
Paulian Le <paulianle7@gmail.com>
2025-11-13 21:42:35 -0800

    Time: 8 ms (36.92%), Space: 10.9 MB (39.95%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        // tech: substring --> sliding window
-            // containing same letter --> dealing w/ freq here --> hash table
+        // tech: 
+            // LONGEST && containing same letter substring --> sliding window
+            // same letter --> deals w/ freq --> hash tables
         
-        // time & space comp: 
-            // time: o(n)
-            // space: o(n)
+        // 1. declare result to return @ end 
+        int result = 0; 
 
-        // 1. declare var to return as result 
-        int longest = 0;
-
-        // 2. declare hash table to track freq 
+        // 2. declare hash table 
         unordered_map<char, int> hashTable;
 
-        // 3. have a variable to track largest freq 
-            // helps us do everything in one-pass 
-            // don't have to constantly check for largest frequency 
+        // 3. declare helper variables 
+            // ensures that we don't have to continuously look for the maxFreq 
         int maxFreq = 0;
 
-        // 4. iterate thr string now --> establishing sliding window tech
+        // 4. perform sliding window technique 
         int leftPtr = 0; 
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // populate hash table first 
-            hashTable[s[rightPtr]]++;
+            // check for valid window?? 
+            hashTable[s[rightPtr]]++; 
+            maxFreq = max(maxFreq, hashTable[s[rightPtr]]);
 
-            // update maxFreq accordingly 
-            maxFreq = max(hashTable[s[rightPtr]], maxFreq);
-
-            // check if window update is needed? 
             while((rightPtr - leftPtr + 1) - maxFreq > k)
             {
                 hashTable[s[leftPtr]]--; 
-
                 leftPtr++;
             }
 
-            longest = max(longest, (rightPtr - leftPtr + 1));
+            result = max(result, (rightPtr - leftPtr + 1));
         }
 
-        return longest;
+        return result;
     }
 };
