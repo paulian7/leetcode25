695bc618886657bdf55919477b26003a819dd0d7
Paulian Le <paulianle7@gmail.com>
2025-11-13 22:29:19 -0800

    Time: 0 ms (100%), Space: 15.2 MB (32.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,39 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // tech: binary search!! bc...
-            // sorted 
-            // rotated 
-            // time comp of o(logn) is wanted
-
-        // 1. declare pointers for binary search 
-        int leftPtr = 0;
+        int leftPtr = 0; 
         int rightPtr = nums.size() - 1; 
-
-        // 2. start binary search algorithm
         while(leftPtr <= rightPtr)
         {
-            // calc midPt 
             int midPt = leftPtr + (rightPtr - leftPtr) / 2;
-
-            // check if midPt could poss be our target 
             if(nums[midPt] == target)
             {
-                return midPt;
+                return midPt; 
             }
 
-            // need to figure out which half to search into 
             if(nums[leftPtr] <= nums[midPt])
             {
-                // want to check if we can stay in this half or not 
-                if(target < nums[leftPtr] || target > nums[midPt])
+                if(target > nums[midPt] || target < nums[leftPtr])
                 {
-                    // want to swap halves
                     leftPtr = midPt + 1;
                 }
                 else 
                 {
-                    // can stay in this half! 
                     rightPtr = midPt - 1;
                 }
             }
@@ -49,8 +34,6 @@ public:
                 }
             }
         }
-
-        // 3. return -1 in the case you don't find the target at all 
-        return -1; 
+        return -1;
     }
 };
