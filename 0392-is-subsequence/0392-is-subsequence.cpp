9b904f78b854537c24184d09c8d2dcd79e0c4e43
Paulian Le <paulianle7@gmail.com>
2025-11-14 12:56:58 -0800

    Time: 0 ms (100%), Space: 8.6 MB (66.41%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int sPtr = 0; 
+        // 1. store length of the respective strings 
+        int sLength = s.size(); 
+        int tLength = t.size(); 
+
+        // 2. declare and initialize pointers 
+        int sPtr = 0;
         int tPtr = 0; 
 
-        int sLength = s.length(); 
-        int tLength = t.length(); 
-
+        // 3. iterate thr the strings 
         while(sPtr < sLength && tPtr < tLength)
         {
+            // either move both pointers (when we find a match)
+            // or move just right ptr (when there is no match)
             if(s[sPtr] == t[tPtr])
             {
-                sPtr++;
-            } 
+                sPtr++; 
+            }
 
-            tPtr++;
+            tPtr++; 
         }
-        return sPtr == sLength;
+
+        // only true is source string completely iterated thr (which means there's a subsequence) 
+        return sPtr == sLength; 
     }
 };
