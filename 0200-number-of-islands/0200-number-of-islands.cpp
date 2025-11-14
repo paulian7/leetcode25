bea2253cde814547775534b6929476294c90610f
Paulian Le <paulianle7@gmail.com>
2025-11-14 12:44:58 -0800

    Time: 29 ms (43.75%), Space: 23.3 MB (28.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,70 +1,53 @@
 class Solution {
-    // member variables -- help us explore all possible 4 directions we can go into
     int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
 public:
     int numIslands(vector<vector<char>>& grid) {
-        // declare var to return @ end
-        int numIslands = 0;
+        int result = 0;
 
-        // check in the case our grid is empty -- row and col 
         if(grid.empty() || grid[0].empty())
         {
             return 0;
         }
 
-        // grab the dimensions of the grid 
         int rows = grid.size(); 
         int cols = grid[0].size(); 
 
-        // iterating thr our grid 
         for(int r = 0; r < rows; r++)
         {
             for(int c = 0; c < cols; c++)
             {
-                // check if we've found a 1 --> aka an island! 
                 if(grid[r][c] == '1')
                 {
                     bfs(grid, r, c); 
-                    numIslands++;
+                    result++;
                 }
             }
         }
 
-        return numIslands;
+        return result;
     }
 
-    // HELPER FUNCTION ==
-    void bfs(vector<vector<char>>& grid, int row, int col) {
-        // 1. declare queue
+    void bfs(vector<vector<char>>& grid, int currRow, int currCol) {
         queue<pair<int, int>> q;
 
-        // 2. push curr cell to queue 
-        q.push({row, col});
+        q.push({currRow, currCol});
 
-        // 3. clear it to avoid reprocessing 
-        grid[row][col] = '0';
+        grid[currRow][currCol] = '0';
 
-        // 4. perform bfs 
         while(!q.empty())
         {
-            // grab curr cell to process 
-            auto cell = q.front();
+            auto cell = q.front(); 
+            q.pop();
 
-            // pop it from queue --
-            q.pop(); 
+            int r = cell.first;
+            int c = cell.second; 
 
-            // grab needed info
-            int rc = cell.first; 
-            int cc = cell.second;
-
-            // check for any neighbors now actually
             for(int i = 0; i < 4; i++)
             {
-                int nr = rc + directions[i][0]; 
-                int nc = cc + directions[i][1];
+                int nr = r + directions[i][0]; 
+                int nc = c + directions[i][1];
 
-                // check if in bounds and if it's an island 
                 if((nr >= 0 && nc >= 0) && (nr < grid.size() && nc < grid[0].size()) && (grid[nr][nc] == '1'))
                 {
                     q.push({nr, nc});
@@ -72,5 +55,5 @@ public:
                 }
             }
         }
-    } 
+    }
 };
