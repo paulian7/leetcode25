0eb10ac108303bf55d94dfcf0252cf9f830e684b
Paulian Le <paulianle7@gmail.com>
2025-11-13 20:38:14 -0800

    Time: 0 ms (100%), Space: 9.7 MB (94.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // tech: two pointers 
-            // let's us compare the front && end of string @ the same time 
-        
-        // time & space comp: 
-            // time: o(n) 
-                // as we're iterating from both ends of the string, all elements iterated at most once
-            // space: o(1) 
-                // no other extra data structures were created, other than the 2 integer vars we declared & initialized
-                // would be a diff story if we declared hash tables 
-        
-        // 1. declare & initialize 2 pointers to respective ends of string 
-        int leftPtr = 0; // start of str
-        int rightPtr = s.length() - 1; // end of str 
+        // 2 pointers 
 
-        // 2. start iterating 
+        // establish pointers 
+        int leftPtr = 0; 
+        int rightPtr = s.length() - 1; 
+
         while(leftPtr < rightPtr)
         {
-            // update pointers accordingly IF not alphanumeric
+            // cond 
             while(leftPtr < rightPtr && !isalnum(s[leftPtr]))
             {
-                leftPtr++; // update left to be valid
+                leftPtr++;
             }
 
             while(rightPtr > leftPtr && !isalnum(s[rightPtr]))
             {
-                rightPtr--; // update right to be valid
+                rightPtr--;
             }
 
-            // check for equivalence now 
-                // ensure that chars are LOWERCASED FIRST before checking!
+            // actual check
             if(tolower(s[leftPtr]) != tolower(s[rightPtr]))
             {
-                return false; // return false immediately if not == 
+                return false;
             }
 
-            // UPDATE POINTERS FOR NEXT ITERATION
+            // update pointers
             leftPtr++; 
             rightPtr--;
         }
 
-        // true -- if while-loop finishes execution 
         return true;
+        
     }
 };
