c71d15d19aa2c806cc079c4c306433ab76e9fc94
Paulian Le <paulianle7@gmail.com>
2025-11-13 21:56:27 -0800

    Time: 0 ms (100%), Space: 9.3 MB (9.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
     bool isValid(string s) {
-        // tech: dealing w/ pairs 
-            // want the pairs in some corr order 
-        
-        // time & space comp: 
-            // time: o(n)
-            // space: o(n)
-        
-        // 1. declare stack
-        stack<char> cStack;
+        // 1. declare our stck 
+        stack<char> cStack; 
 
-        // 2. declare hash table to cross reference 
+        // 2. declare hash table
         unordered_map<char, char> closeToOpen = {
             {')', '('}, 
-            {'}', '{'}, 
+            {'}', '{'},
             {']', '['}
         };
 
-        // 3. iterate thr string 
+        // 3. start iterating thr string now! 
         for(char c : s)
         {
-            // check dealing w/ close or open paren
+            // check are we dealing w/ the closing part of the pair?
             if(closeToOpen.count(c))
             {
-                // dealing w/ closed -- 
-                // check if have match or not
+                // start seeing if we've correctly closed the pair
                 if(!cStack.empty() && cStack.top() == closeToOpen[c])
                 {
-                    // got a match! pop the open paren now 
-                    cStack.pop(); 
+                    cStack.pop(); // pop its matching pair off
                 }
                 else 
                 {
-                    // no match :( --> return false immediately 
-                    return false; 
+                    return false; // incorrect closing 
                 }
             }
             else 
             {
-                // dealing w/ open 
+                // dealing w/ open part of the pair
                 cStack.push(c);
             }
         }
 
-        // 4. if stack empty --> valid :) 
         return cStack.empty();
     }
 };
