7b38b2b85fffceb679a0e3c6e7cee34f493c49e1
Paulian Le <paulianle7@gmail.com>
2025-11-13 21:38:52 -0800

    Time: 19 ms (29.65%), Space: 14.2 MB (35.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,38 +2,28 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         // tech: 
-            // substring --> sliding window
-            // want to identify duplicate chars --> hash set 
-                // can use to hold only unique characters so far 
-        
-        // time & space comp: 
-            // time: o(n)
-            // space: o(n)
-        
-        // 1. declare result to return @ end 
-        int result = 0;
-
-        // 2. declare hash set to hold only unique chars
+            // substring - want longest AND NO duplicate chars --> sliding window
+        int longest = 0;
+        // 1. declare hash set 
+            // can track OUR UNIQUE values
         unordered_set<char> seenChars;
 
-        // 3. execute sliding window technique 
+        // 2. set up pointers for our sliding window --> which will track our valid substring
         int leftPtr = 0; 
         for(int rightPtr = 0; rightPtr < s.length(); rightPtr++)
         {
-            // check if window is still valid
+            // check if window is valid 
             while(seenChars.find(s[rightPtr]) != seenChars.end())
             {
-                // invalid --> we've found a duplicate within our substring
-                // need to shrink window w/ leftPtr til window becomes valid again 
                 seenChars.erase(s[leftPtr]);
                 leftPtr++;
             }
 
             // else... 
             seenChars.insert(s[rightPtr]);
-            result = max(result, (rightPtr - leftPtr + 1));
+            longest = max(longest, (rightPtr - leftPtr + 1));
         }
 
-        return result;
+        return longest;
     }
 };
