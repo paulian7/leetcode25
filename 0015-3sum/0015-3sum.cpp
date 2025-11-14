cb3338d36abdbad5f16d11bd1c53561393bffedc
Paulian Le <paulianle7@gmail.com>
2025-11-13 20:47:52 -0800

    Time: 39 ms (94.32%), Space: 29.1 MB (72.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,56 +2,66 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         // tech: 
-            // want 3 vals adding up to a specific sum (i.e 0) 
-            // can utilize 2 pointers here 
+            // want 3 vals that add up to 0
+            // want to ensure no duplicate triplets are found 
         
         // time & space comp: 
-            // time: simplifies down to o(n^2)
-                // o(nlogn) -- bc use sort()
-                // o(n^2) -- bc of the nested loops
+            // time: o(n logn) + o(n^2)
+                // simplifies down to o(n^2) 
+                // sort() - o(nlogn) 
+                // nested loops - o(n^2) 
             // space: 
-                // o(m) -- if we want to count the triplets in output array
+                // if you count sort: o(1) 
+                    // c++ does in-place 
+                // output array: o(m) 
+                    // m - num of triplets
         
-        // 1. sort -- want to avoid duplicates 
+        // 1. sort! --> time comp; o(n logn) :(
         sort(nums.begin(), nums.end());
 
-        // 2. iterate thr array -- also declaring array for our result 
-        vector<vector<int>> result;
+        vector<vector<int>> result; // will hold our final answer of triplets
+
+        // 2. start iterating thr 
         for(int i = 0; i < nums.size(); i++)
         {
-            // deal w/ 1st val -- ensure no duplicates 
-            if(i > 0 && nums[i] == nums[i - 1]) continue; 
+            // deal w/ the first val in our triplet first 
+            if(i > 0 && nums[i] == nums[i - 1]) continue; // skip to next iteration bc dup
 
-            // else, set up 2-pointers method 
+            // otherwise, deal w/ our other 2 vals now 
+                // will be executing the 2 pointers method here to do so 
             int leftPtr = i + 1; 
             int rightPtr = nums.size() - 1; 
             while(leftPtr < rightPtr)
             {
-                // check condition 
+                // get the curr triplet val 
                 int currSum = nums[i] + nums[leftPtr] + nums[rightPtr]; 
+
+                // adjust the pointers accordingly (based on the currSum)
                 if(currSum > 0)
                 {
-                    rightPtr--; 
+                    rightPtr--; // needs to lower down bc it's pointing at a too big val
                 }
                 else if(currSum < 0)
                 {
-                    leftPtr++; 
+                    leftPtr++; // need to increase leftPtr bc it's pointing at too small
                 }
                 else 
                 {
-                    // currSum is actually equal to 0 --> so add to result 
+                    // we finally have a sum of 0! add it to our result vector 
                     result.push_back({nums[i], nums[leftPtr], nums[rightPtr]});
 
-                    // update pointer now 
+                    // now update our pointers -- dont need to necessarily update both
                     leftPtr++; 
+
+                    // ensure after the update of leftPtr... we haven't hit a dup! :(
                     while(leftPtr < rightPtr && nums[leftPtr] == nums[leftPtr - 1])
                     {
-                        leftPtr++; 
+                        leftPtr++;
                     }
                 }
             }
         }
 
-        return result;
+        return result; 
     }
 };
