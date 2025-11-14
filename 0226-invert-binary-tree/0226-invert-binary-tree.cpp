59c2b3725a5ea1e543c43f9138c4b1968b53d06a
Paulian Le <paulianle7@gmail.com>
2025-11-13 23:36:44 -0800

    Time: 0 ms (100%), Space: 12.5 MB (23.42%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,30 +12,21 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        // tech: recursive depth first search (dfs) 
-            // going thr left subtree(s) first then right subtree(s) 
-        
-        // time & space comp: 
-            // time: o(n) 
-                // iterating node by node 
-            // space: o(n) 
-                // o(n), worst case - unbalanced tree 
-                // o(logn), best case - balanced tree 
-        
-        // 1. establish a base case - as you would w/ any recursive technique 
+        // dfs 
+
+        // 1. base case 
         if(!root)
         {
             return nullptr;
         }
 
-        // 2. op - start swapping 
+        // 2. op 
         swap(root -> left, root -> right);
 
-        // 3. split overall problem - into subproblems -> just the left and right subtree(s)
+        // 3. split into subproblems 
         invertTree(root -> left); 
         invertTree(root -> right); 
 
-        // 4. return completed invert tree 
         return root;
     }
 };
