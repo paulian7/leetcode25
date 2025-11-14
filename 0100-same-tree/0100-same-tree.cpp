42fd69233a99bb8475bab498515bed8042cb140d
Paulian Le <paulianle7@gmail.com>
2025-11-13 23:48:14 -0800

    Time: 0 ms (100%), Space: 12.8 MB (77.81%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,37 +12,22 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // tech: recursive depth first search (dfs) 
-            // need same vals && structure 
+        // tech: recursive dfs 
         
-        // time & space comp: 
-            // time: o(p + q) 
-                // p - num of nodes in 'p' tree 
-                // q - num of nodes in 'q' tree 
-            // space: o(n) -- mainly comes from our call stack that's used during recursion techniques 
-                // o(n) - due to unbalanced tree, worst case 
-                    // n -- number of items in our call stack 
-                // o(logn) - balanced tree, best case 
-        
-        // 1. start w/ a base case as always w/ recursion 
-            // base case - is when we have an empty tree --> meaning tech equal 
+        // establish base cases 
         if(!p && !q)
         {
             return true;
         }
 
-        // 2. else, do operation -- check for equivalance --> then split into subproblems
         if((p && q) && (p -> val == q -> val))
         {
-            // perform recursive calls -- 
-                // on left subtree of p && q 
-                // on right subtree of p && q 
             return isSameTree(p -> left, q -> left) && 
-                    isSameTree(p -> right, q -> right);
+                isSameTree(p -> right, q -> right);
         }
         else 
         {
-            return false; // no matching vals
+            return false;
         }
     }
 };
