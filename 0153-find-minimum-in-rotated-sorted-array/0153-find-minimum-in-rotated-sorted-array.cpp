1b31b36bc21bca40b00c28bea8d56b0a9593e2cb
Paulian Le <paulianle7@gmail.com>
2025-11-13 22:24:43 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // tech: binary search 
-            // sorted, rotated, time comp of logn is wanted
+        // declare result to return @ end
+        int minVal = nums[0];
 
-        // 1. declare result to return 
-        int minVal = nums[0]; 
-
-        // 2. set up binary search 
-        int leftPtr = 0; 
+        // declare pointers for binary search 
+        int leftPtr = 0;
         int rightPtr = nums.size() - 1;
+
+        // perform binary search 
         while(leftPtr <= rightPtr)
         {
-            // check if in sorted portion immediately 
+            // check if we're already in a sorted part of the array 
             if(nums[leftPtr] < nums[rightPtr])
             {
                 minVal = min(minVal, nums[leftPtr]); 
                 break;
             }
 
-            // binary search steps...
-            int midPt = leftPtr + (rightPtr - leftPtr) / 2;
-            // check if midPt's val could be the smallest val
-            minVal = min(minVal, nums[midPt]);
-
-            // check for which portion to look into now 
+            // need to figure out which half! 
+            int midPt = leftPtr + (rightPtr - leftPtr) / 2; 
             if(nums[leftPtr] <= nums[midPt])
             {
+                // apart of increasing section.... want to switch to smaller vals
                 leftPtr = midPt + 1;
             }
             else 
             {
+                // want to go left to see smaller vals
                 rightPtr = midPt - 1;
             }
         }
