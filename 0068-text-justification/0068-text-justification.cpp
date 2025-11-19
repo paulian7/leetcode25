3baefc60971e1be788d850b86c102e977e316ea3
Paulian Le <paulianle7@gmail.com>
2025-11-18 20:45:57 -0800

    Time: 0 ms (100%), Space: 10.9 MB (12.57%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,85 +1,92 @@
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        // 1. declare result to return 
-            // where each val in array will be a line
-        vector<string> res; 
+        // time & space comp: o(n * m) 
+            // n - num of words 
+            // m - avg length of the words
+        
+        // 1. ====== declare starting variables ======
+        vector<string> result; // final answer, each element is a "line" 
+        vector<string> currLine; // holds all words in the current line 
+        int currLength = 0; // holds length of all the words in the currLine 
+        int i = 0; // our iterator, iterates word-by-word thr input array
 
-        // 2. declare var for curr line
-        vector<string> line;
-
-        // 3. declare helper variables
-            // length -- tracks length of curr line
-            // i - pointer 
-                // help us iterate thr input
-        int length = 0;
-        int i = 0;
-
-        // 3. start iterating 
+        // 2. ====== iterate thr input loop, justifying ======
+            // loop will break once we get to the last word... 
+            // ... in which we need to deal w/ trailing spaces
         while(i < words.size())
         {
-            // checking if the curr line is complete
-                // take length of current line + length of another word + line.size() 
-                    // line.size() -- acounts for the spaces, as each word needs a space after it
-            if(length + words[i].size() + line.size() <= maxWidth)
+            // == case 1 - when the line isn't complete yet == 
+            if(currLength + words[i].size() + currLine.size() <= maxWidth)
             {
-                // ====== when the line isn't complete ======
-                    // will need to update our curr line
-                // add curr word to line 
-                line.push_back(words[i]); 
-                // update length of line 
-                length += words[i].size(); 
-                // increment 'i' pointer to move forward to next word in input array
-                i++;
+                // can still add another word into curr line... 
+                currLine.push_back(words[i]); 
+                currLength += words[i].length(); 
+                i++; // update iterator to go to next word 
             }
             else 
             {
-                // ====== when the line IS complete ======
-                // calc the number of spaces we need to divide evenly in the line
-                    // rmbr - "length" variable does not include the spaces -- which is what makes this computation possible
-                int extraSpace = maxWidth - length;
+                // == case 2 - when the line IS complete == 
+                    // will need to evenly distribute spaces -> then add to final result
 
-                // calc how we're going to divide the spaces 
-                int remainder = extraSpace % max(1, (int)(line.size() - 1));
-                int space = extraSpace / max(1, (int)(line.size() - 1));
+                // ----- deal w/ all the "space calculations" now -----
+                int extraSpace = maxWidth - currLength; // all the space we still need to fill 
 
-                // add in the spaces
-                    // line.size() - line contains all words in curr line
-                        // subtract by 1 bc we don't have a space @ the end of last word 
-                        // only adding spaces in btwn words
-                for (int j = 0; j < max(1, (int)line.size() - 1); j++) 
+                // calc num of spaces to put in btwn words specifically 
+                    // how much spaces go in btwn a pair of words
+                int space = extraSpace / max(1, (int)(currLine.size() - 1));
+                
+                // calc any spaces left over that we still need to add 
+                int remainderSpaces = extraSpace % max(1, (int)(currLine.size() - 1));
+
+                // ----- add all the spaces btwn the words in the line now -----
+                for (int j = 0; j < max(1, (int)(currLine.size() - 1)); j++)
                 {
-                    line[j] += string(space, ' ');
-                    if (remainder > 0) 
+                    // add num of spaces needed after curr word
+                    currLine[j] += string(space, ' ');
+                    // any remainder spaces? 
+                        // always 1 remainder added @ each iteration
+                    if(remainderSpaces > 0)
                     {
-                        line[j] += " ";
-                        remainder--;
+                        currLine[j] += ' ';
+                        remainderSpaces--;
                     }
                 }
 
-                string justifiedLine = accumulate(line.begin(), line.end(), string());
-                res.push_back(justifiedLine);
-                line.clear();
-                length = 0;
+                // ----- finally add currLine to final result -----
+                // mush everything together into a string
+                string justifiedLine = accumulate(currLine.begin(), currLine.end(), string());
+                // add to final result 
+                result.push_back(justifiedLine); 
+                // reset variables for next line 
+                currLine.clear(); 
+                currLength = 0;
             }
         }
 
-        // ====== in the case we're HANDLING THE VERY LAST LINE ======
-        // take each word and join them together w/ a space in btwn them
-        string lastLine = accumulate(line.begin(), line.end(), string(),
-                                      [](string a, string b) {
-                                            return a.empty() ? b : a + " " + b;
-                                        });
+        // 3. ====== deal w/ the last line that needs to be left-justified ======
+        // add 1 space in btwn each word 
+        string lastLine;
+        for(int k = 0; k < currLine.size(); k++)
+        {
+            if(k < currLine.size() - 1)
+            {
+                lastLine += currLine[k] + ' '; // want space to separate itself w/ the next word coming
+            }
+            else 
+            {
+                lastLine += currLine[k]; // last word, no want space 
+            }
+        }
 
-        // how to calc the # of spaces left over after joining all the words together w/ a space?
-        int trailingSpace = maxWidth - lastLine.size(); 
-        // now add trailing space to the end of our "lastLine"
-        lastLine += string(trailingSpace, ' ');
-        
-        // now add lastLine to our result 
-        res.push_back(lastLine);
+        // add in any trailing spaces
+        int trailingSpaces = maxWidth - lastLine.length(); 
+        lastLine += string(trailingSpaces, ' ');
 
-        // ====== RETURN FINAL ANSWER ======
-        return res;
+        // finally add lastLine to result
+        result.push_back(lastLine);
+
+        // 4. ====== return final result ======
+        return result;
     }
 };
