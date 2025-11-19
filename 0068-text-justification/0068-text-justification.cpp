7137cc26faf4a6aff4de53c518d53a73ef6ab99b
Paulian Le <paulianle7@gmail.com>
2025-11-18 22:23:10 -0800

    Time: 0 ms (100%), Space: 10.9 MB (12.57%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,58 +1,48 @@
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        // declare starting variables 
         vector<string> result; 
-        vector<string> currLine;
-        int currLength = 0; 
+        vector<string> currLine; 
+        int currLength = 0;
         int i = 0; 
 
-        // iterate thr input until we reach the last line
         while(i < words.size())
         {
             if(currLength + words[i].length() + currLine.size() <= maxWidth)
             {
-                currLine.push_back(words[i]); 
+                currLine.push_back(words[i]);
                 currLength += words[i].length(); 
-                i++; 
+                i++;
             }
             else 
             {
-                // need to add spaces btwn all the words now 
-
-                // deal w/ space calculations
                 int extraSpace = maxWidth - currLength;
                 int spaces = extraSpace / max(1, (int)(currLine.size() - 1));
                 int remainder = extraSpace % max(1, (int)(currLine.size() - 1));
 
-                // actually add spaces after each word now
                 for(int j = 0; j < max(1, (int)(currLine.size() - 1)); j++)
                 {
                     currLine[j] += string(spaces, ' '); 
                     if(remainder > 0)
                     {
-                        currLine[j] += ' ';
+                        currLine[j] += ' '; 
                         remainder--;
                     }
                 }
 
-                // add to answer 
-                string justifiedLine = accumulate(currLine.begin(), currLine.end(), string());
-                result.push_back(justifiedLine); 
+                string justifiedLine = accumulate(currLine.begin(), currLine.end(), string()); 
+                result.push_back(justifiedLine);
                 currLine.clear(); 
                 currLength = 0;
             }
         }
 
-        // dealing w/ last line 
         string lastLine; 
-        // want to ensure we have spaces in btwn each word 
         for(int k = 0; k < currLine.size(); k++)
         {
-            // if not dealing w/ last element, add a ' ' after the curr word 
-            if(k < currLine.size() - 1)
+            if(k < currLine.size() - 1) 
             {
-                lastLine += currLine[k] + ' ';
+                lastLine += currLine[k] + ' '; 
             }
             else 
             {
@@ -60,14 +50,9 @@ public:
             }
         }
 
-        // deal w/ trailing spaces 
         int trailingSpaces = maxWidth - lastLine.length(); 
-        // add the trailing spaces to the last line 
-        lastLine += string(trailingSpaces, ' '); 
-        // add final line to result 
+        lastLine += string(trailingSpaces, ' ');
         result.push_back(lastLine); 
-
-        // return final answer
         return result;
     }
 };
