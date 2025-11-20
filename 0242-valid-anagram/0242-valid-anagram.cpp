c2e182fbc11c7bbc3fd78ebf7bb1e3e7e27da7a3
Paulian Le <paulianle7@gmail.com>
2025-11-19 22:55:51 -0800

    Time: 1 ms (59.26%), Space: 9.8 MB (47.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // tech: anagram => share same character frequency
+        // time: o(n + m) 
+        // space: o(1) 
 
-        // time & space comp: 
-            // time: o(n) 
-            // space: o(n)
-        
-        // 1. deal w/ edge case first! 
+        // edge case 
         if(s.length() != t.length())
         {
             return false;
         }
 
-        // 2. declare hash tables to track char frequencies 
         unordered_map<char, int> hashS; 
         unordered_map<char, int> hashT; 
 
-        // 3. populate hash tables 
         for(int i = 0; i < s.length(); i++)
         {
             hashS[s[i]]++; 
             hashT[t[i]]++; 
         }
 
-        // 4. return whether or not all key-value pairs match up 
         return hashS == hashT; 
     }
 };
