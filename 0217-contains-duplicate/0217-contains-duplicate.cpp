1e5c454a80ef5bdde206c162bb0aa0ea79b61775
Paulian Le <paulianle7@gmail.com>
2025-11-19 22:52:25 -0800

    Time: 55 ms (36.64%), Space: 90.9 MB (30.24%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // tech: 
-            // want to identify if a val appears TWICE 
-            // hash sets -- perfect for checking for existence of an element 
-                // takes o(1) time to search 
-        
         // time & space comp: 
-            // time: o(n)
-            // space: o(n)
-        
-        // 1. declare hash set 
-        unordered_set<int> hashSet; 
+            // time: o(n) 
+            // space: o(n) 
 
-        // 2. iterate thr array 
+        unordered_set<int> seenChars; 
+
         for(int val : nums)
         {
-            // check for any duplicates immediately 
-            if(hashSet.find(val) != hashSet.end())
+            if(seenChars.find(val) != seenChars.end())
             {
                 return true;
             }
 
-            // if not dup, add to hashSet for later reference 
-            hashSet.insert(val);
+            seenChars.insert(val);
         }
 
         return false;
