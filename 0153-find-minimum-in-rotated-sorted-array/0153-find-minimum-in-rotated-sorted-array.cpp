056258af09bf543696c454797ca0507252a0c2e6
Paulian Le <paulianle7@gmail.com>
2025-11-20 20:42:22 -0800

    Time: 0 ms (100%), Space: 14.2 MB (47.67%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        // tech: 
-            // binary search - rotated, sorted, time comp of logn
-        
-        // time & space comp: 
-            // time: o(logn)
-                // w/ each iteration - the search space is HALVED
-            // space: o(1) 
-        
-        // 1. declare result to return final answer
-        int minVal = nums[0]; 
+        // time: o(logn) 
+        // space: o(1) 
 
-        // 2. set up pointers for binary search
+        int minVal = nums[0];
+
         int leftPtr = 0; 
-        int rightPtr = nums.size() - 1; 
-
-        // 3. execute binary search 
+        int rightPtr = nums.size() - 1;
         while(leftPtr <= rightPtr)
         {
-            // check case -
             if(nums[leftPtr] < nums[rightPtr])
             {
                 minVal = min(minVal, nums[leftPtr]);
                 break;
             }
 
-            // else, need to find where to search 
-                // ensure that there's no integer overflow by doing it this way 
             int midPt = leftPtr + (rightPtr - leftPtr) / 2;
-            minVal = min(minVal, nums[midPt]);
-            
-            if(nums[leftPtr] <= nums[midPt])
+            minVal = min(nums[midPt], minVal); 
+
+            if(nums[midPt] >= nums[leftPtr])
             {
-                // the increasing section --> want to swap to small section 
-                leftPtr = midPt + 1; 
+                leftPtr = midPt + 1;
             }
             else 
             {
-                rightPtr = midPt - 1; 
+                rightPtr = midPt - 1;
             }
         }
 
