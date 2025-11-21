7882f66db51be9dacad880dfaff39dad84c9d105
Paulian Le <paulianle7@gmail.com>
2025-11-20 20:46:40 -0800

    Time: 0 ms (100%), Space: 31.3 MB (45.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // establish pointers 
-        int leftPtr = 0; 
-        int rightPtr = nums.size() - 1; 
+        int leftPtr = 0;
+        int rightPtr = nums.size() - 1;
 
-        // perform binary search 
         while(leftPtr <= rightPtr)
         {
-            // calc midPt 
             int midPt = leftPtr + (rightPtr - leftPtr) / 2;
-
-            // check if midPt is == target 
             if(nums[midPt] == target)
             {
                 return midPt; 
             }
-
-            // figure out which half to check
+            
             if(target > nums[midPt])
             {
-                leftPtr = midPt + 1; 
+                leftPtr = midPt + 1;
             }
             else 
             {
