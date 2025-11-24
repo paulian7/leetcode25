475927f0d590f2792cdd16c4977475914497c229
Paulian Le <paulianle7@gmail.com>
2025-11-23 18:14:12 -0800

    Time: 0 ms (100%), Space: 63 MB (47.95%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // declare result to return @ the end 
+        // tech: 
+            // 2 pointers method! 
+                // want to find largest area 
+                // can maximize length by having 2 pointers at beg & end 
+        
+        // time & space comp: 
+            // time: o(n)
+            // space: o(1)
+        
+        // 1. declare var to return @ end 
         int maxArea = 0; 
 
-        // declare pointers for 2 pointers method 
+        // 2. execute 2 pointers method 
         int leftPtr = 0; 
         int rightPtr = height.size() - 1; 
+
         while(leftPtr < rightPtr)
         {
-            // cond 
+            // 3. check for curr area 
+                // length * height 
+                    // want to minimize the height so no overflow occurs 
             int currArea = (rightPtr - leftPtr) * min(height[leftPtr], height[rightPtr]);
-            // update maxArea accordingly
-            maxArea = max(maxArea, currArea); 
 
-            // update pointers accordingly 
+            // 4. update maxArea accordingly 
+            maxArea = max(maxArea, currArea);
+
+            // 5. see if we can maximize the heights now by adjusting the pointers 
             if(height[leftPtr] < height[rightPtr])
             {
-                leftPtr++; 
+                leftPtr++; // update left bc left too small 
             }
             else if(height[rightPtr] < height[leftPtr])
             {
-                rightPtr--;
+                rightPtr--; // update rightPtr bc right too small 
             }
             else 
             {
-                // pick a random pointer to update 
+                // both heights are the same val... can just update one height then 
                 leftPtr++;
             }
         }
 
+        // return result 
         return maxArea;
     }
 };
