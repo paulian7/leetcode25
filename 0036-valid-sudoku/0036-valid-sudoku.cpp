b2e6b5c646c62e7f4947f98b37360d7be5c3d08f
Paulian Le <paulianle7@gmail.com>
2025-11-23 17:39:53 -0800

    Time: 20 ms (5.59%), Space: 26.2 MB (5.15%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // tech: lots of hash sets here! 
-            // checking if val already in that row 
-            // if val already in that col
-            // if val already in the 3x3 square 
+        // tech: hash sets -> easy to identify duplicates in: 
+            // the curr row 
+            // the curr col
+            // the 3x3 it's in
+        
+        // time & space comp:  
+            // time:
+            // space: 
+        
+        // 1. declare hash table for entire row && col within 9x9 grid
+        unordered_map<int, unordered_set<char>> row;
+        unordered_map<int, unordered_set<char>> col;
 
-        // 1. declare row and column respec hash table
-            // key: column or row number in overall 9x9 
-            // value: all values in that column or row
-        unordered_map<int, unordered_set<char>> rows;
-        unordered_map<int, unordered_set<char>> cols;
+        // 2. declare a separate map to track each 3x3 grid respectively
+        map<pair<int, int>, unordered_set<char>> square;
 
-        // 2. create a map - tracks each of the 3x3 squres
-            // maps - maintain order; lookups slower than hashtables (takes logn)
-            // key: set of grid coordinates (row, col) 
-            // val: tracks chars already in that spec square 
-        map<pair<int, int>, unordered_set<char>> squares;
+        // 3. iterate thr the 9x9 grid now
+        // iterate thr each row
+        for(int r = 0; r < 9; r++)
+        {
+            // then iterate thr each col
+            for(int c = 0; c < 9; c++)
+            {
+                // check for empty boxes (.) --> skip to next iteration if empty
+                if(board[r][c] == '.') continue;
 
-        // 3. iterate thr entire grid now (9x9 dimensions)
-            // have 9 rows, 9 cols 
-        for (int r = 0; r < 9; r++) {
-            // iterate thr each col within the row 
-            for (int c = 0; c < 9; c++) {
-                // check if any empty spaces --> yes, skip to next col in row (aka go to next iteration)
-                if (board[r][c] == '.') continue;
-                
-                // identify what 3x3 grid we're dealing w/ this curr val 
+                // check now for what 3x3 grid it is in the overall 9x9 
                 pair<int, int> squareKey = {r / 3, c / 3};
 
-                // 1) check if curr char is seen in row...
-                // 2) or entire column 
-                // 3) or within the entire 3x3 grid it's in 
-                // ... then invalid suduko! 
-                if (rows[r].count(board[r][c]) || cols[c].count(board[r][c]) || squares[squareKey].count(board[r][c])) {
+                // else, check for duplicates in the 3 scenarios
+                if(row[r].count(board[r][c]) || col[c].count(board[r][c]) || square[squareKey].count(board[r][c]))
+                {
                     return false;
                 }
 
-                // otherwise... curr val is VALID! --> now update our trackers~ 
-                    // will help us ensure that this currVal won't appear again in the...
-                        // .. same row / col / grid 
-                rows[r].insert(board[r][c]);
-                cols[c].insert(board[r][c]);
-                squares[squareKey].insert(board[r][c]);
+                // if no dups, add it to the hashSets for later reference 
+                row[r].insert(board[r][c]);
+                col[c].insert(board[r][c]);
+
+                square[squareKey].insert(board[r][c]);
             }
         }
 
-        // passed all tests --> valid suduko! 
+        // if all iterations finished, passed tests -> valid suduko
         return true;
     }
 };
