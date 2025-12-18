f0a03cd7a301db9b99bfbc7c2e49ba7ad0ecea74
Paulian Le <paulianle7@gmail.com>
2025-12-17 20:19:31 -0800

    Time: 3 ms (39.57%), Space: 16 MB (50.59%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        // time & space comp: 
-            // time: o(n logn) 
-                // nlogn - sort() 
-                // n - iteratin thr -> but overall simplifies down to just nlogn
-            // space: o(1) - sort happens in-place
-        
-        // 0. cover edge case 
         if(intervals.empty())
         {
             return true;
         }
 
-        // 1. sort intervals by startPt
         sort(intervals.begin(), intervals.end());
-
-        // 2. iterate thr sorted intervals
-            // start at i = 1 bc we want to check...
-            // ... 2nd interval's startPt against 1st interval's endPt
         for(int i = 1; i < intervals.size(); i++)
         {
             if(intervals[i][0] < intervals[i - 1][1])
