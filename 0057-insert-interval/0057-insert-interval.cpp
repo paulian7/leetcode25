3636f294648af235e09a11d03414b30495aa58a4
Paulian Le <paulianle7@gmail.com>
2025-12-18 14:14:59 -0800

    Time: 0 ms (100%), Space: 21.7 MB (73.19%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        // time & space comp: 
-            // time: o(n)
-            // space: o(1) 
-                // unless you count output list --> then o(n)
-                
-        // 1. start off w/ helper variables && declaring a result to return @ the end
+        vector<vector<int>> fixedIntervals;
+
         int numIntervals = intervals.size(); 
-        int i = 0; // our iterator
-
-        vector<vector<int>> fixedIntervals; // result
-
-        // 2. iterate thr the intervals -- go thr case by case 
-
-        // CASE 1 == push all the intervals that come BEFORE the new interval first
-            // these intervals are the one's that end BEFORE the new interval
-            // does not overlap w/ the newInterval @ all
-            // ends when we find we reach end of list OR find a val that might overlap w/ newInterval
+        int i = 0; 
+        
         while(i < numIntervals && intervals[i][1] < newInterval[0])
         {
             fixedIntervals.push_back(intervals[i]);
             i++;
         }
 
-        // CASE 2 == deal w/ overlapping interval(s)
-            // as end of new interval is NOT before the start of the currInterval
-            // ... which means there's some overlap occurin somehow
         while(i < numIntervals && newInterval[1] >= intervals[i][0])
         {
             newInterval[0] = min(newInterval[0], intervals[i][0]);
@@ -36,7 +21,6 @@ public:
         }
         fixedIntervals.push_back(newInterval);
 
-        // CASE 3 == add any leftover interval(s)
         while(i < numIntervals)
         {
             fixedIntervals.push_back(intervals[i]);
